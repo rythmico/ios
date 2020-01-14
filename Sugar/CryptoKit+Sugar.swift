66a7055fc2b48bf6459718db3a122e6fae3ae427
davdroman <d@vidroman.dev>
2020-01-14 01:42:15 +0000

    Add CryptoKit sugar

---
+++ Sugar/CryptoKit+Sugar.swift
@@ -0,0 +1,18 @@
+import Foundation
+import CryptoKit
+
+extension HashFunction {
+    static func hash(utf8String: String) -> Self.Digest {
+        hash(data: Data(utf8String.utf8))
+    }
+
+    static func hashString(utf8String: String) -> String {
+        hashString(data: Data(utf8String.utf8))
+    }
+
+    static func hashString<D: DataProtocol>(data: D) -> String {
+        hash(data: data)
+            .compactMap { String(format: "%02x", $0) }
+            .joined()
+    }
+}
