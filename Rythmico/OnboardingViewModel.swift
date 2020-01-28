1e70f3d9deca9c6f932a06edcac280718b21674a
davdroman <d@vidroman.dev>
2020-01-28 23:15:22 +0000

    Implement onboarding and main tab screens

---
+++ Rythmico/OnboardingViewModel.swift
@@ -0,0 +1,88 @@
+import Foundation
+import Combine
+
+protocol OnboardingViewModelProtocol: ViewModel where ViewData == OnboardingViewData {
+    func showAppleAuthenticationSheet()
+}
+
+final class OnboardingViewModel: OnboardingViewModelProtocol {
+    let objectWillChange = ObservableObjectPublisher()
+
+    private(set) var viewData = OnboardingViewData() {
+        willSet { dispatchQueue?.async(execute: objectWillChange.send) ?? objectWillChange.send() }
+    }
+
+    private let appleAuthorizationService: AppleAuthorizationServiceProtocol
+    private let authenticationService: AuthenticationServiceProtocol
+    private let keychain: KeychainProtocol
+    private let dispatchQueue: DispatchQueue?
+
+    init(
+        appleAuthorizationService: AppleAuthorizationServiceProtocol,
+        authenticationService: AuthenticationServiceProtocol,
+        keychain: KeychainProtocol,
+        dispatchQueue: DispatchQueue?
+    ) {
+        self.appleAuthorizationService = appleAuthorizationService
+        self.authenticationService = authenticationService
+        self.keychain = keychain
+        self.dispatchQueue = dispatchQueue
+    }
+
+    func showAppleAuthenticationSheet() {
+        appleAuthorizationService.requestAuthorization { result in
+            switch result {
+            case .success(let credential):
+                self.keychain.appleAuthorizationUserId = credential.userId
+                self.viewData.isLoading = true
+                self.authenticationService.authenticateAppleAccount(with: credential) { result in
+                    self.viewData.isLoading = false
+                    switch result {
+                    case .success:
+                        // Firebase's Auth singleton makes this line redundant since it's notifies listeners
+                        // about user changes upon sign in. However if services are changed there's
+                        // a chance this line might be needed.
+                        // self.authenticationStatusObserver.statusDidChangeHandler(accessTokenProvider)
+                        break
+                    case .failure(let error):
+                        self.handleAuthenticationError(error)
+                    }
+                }
+            case .failure(let error):
+                self.handleAuthorizationError(error)
+            }
+        }
+    }
+
+    private func handleAuthorizationError(_ error: AppleAuthorizationService.Error) {
+        switch error.code {
+        case .notHandled:
+            fatalError(error.localizedDescription)
+        case .canceled, .failed, .invalidResponse, .unknown:
+            break
+        @unknown default:
+            break
+        }
+    }
+
+    private func handleAuthenticationError(_ error: AuthenticationServiceProtocol.Error) {
+        let errorMessage: String?
+        switch error.reasonCode {
+        case .unknown,
+             .networkError,
+             .tooManyRequests:
+            errorMessage = nil
+        case .invalidAPIKey,
+             .appNotAuthorized,
+             .internalError,
+             .operationNotAllowed:
+            fatalError("\(error.localizedDescription) (\(error.reasonCode.rawValue))")
+        case .invalidCredential,
+             .userDisabled,
+             .invalidEmail,
+             .missingOrInvalidNonce:
+            errorMessage = "\(error.localizedDescription) (\(error.reasonCode.rawValue))"
+        }
+        viewData.errorMessage = errorMessage
+    }
+}
---
+++ Rythmico/RootViewModel.swift
@@ -0,0 +1,65 @@
+import Foundation
+import AuthenticationServices
+import Combine
+
+protocol RootViewModelProtocol: ViewModel where ViewData == RootViewData {}
+
+final class RootViewModel: RootViewModelProtocol {
+    let objectWillChange = ObservableObjectPublisher()
+
+    private(set) var viewData: RootViewData {
+        willSet { dispatchQueue?.async(execute: objectWillChange.send) ?? objectWillChange.send() }
+    }
+
+    private let authorizationCredentialStateProvider: AppleAuthorizationCredentialStateProvider
+    private let authorizationCredentialRevocationObserving: AppleAuthorizationCredentialRevocationObserving
+    private let authenticationAccessTokenProviderObserving: AuthenticationAccessTokenProviderObserving
+    private let dispatchQueue: DispatchQueue?
+
+    init(
+        keychain: KeychainProtocol,
+        onboardingViewModel: OnboardingViewModel,
+        authorizationCredentialStateProvider: AppleAuthorizationCredentialStateProvider,
+        authorizationCredentialRevocationObserving: AppleAuthorizationCredentialRevocationObserving,
+        authenticationAccessTokenProviderObserving: AuthenticationAccessTokenProviderObserving,
+        deauthenticationService: DeauthenticationServiceProtocol,
+        dispatchQueue: DispatchQueue?
+    ) {
+        func viewData(for provider: AuthenticationAccessTokenProvider?) -> ViewData {
+            if let provider = provider {
+                return .authenticated(MainTabView(viewModel: MainTabViewModel(accessTokenProvider: provider)))
+            } else {
+                return .unauthenticated(OnboardingView(viewModel: onboardingViewModel))
+            }
+        }
+
+        self.viewData = viewData(for: nil)
+        self.authorizationCredentialStateProvider = authorizationCredentialStateProvider
+        self.authorizationCredentialRevocationObserving = authorizationCredentialRevocationObserving
+        self.authenticationAccessTokenProviderObserving = authenticationAccessTokenProviderObserving
+        self.dispatchQueue = dispatchQueue
+
+        self.authenticationAccessTokenProviderObserving.statusDidChangeHandler = { provider in
+            self.viewData = viewData(for: provider)
+        }
+
+        if let authorizationUserId = keychain.appleAuthorizationUserId {
+            authorizationCredentialStateProvider.getCredentialState(forUserID: authorizationUserId) { state in
+                switch state {
+                case .revoked, .transferred:
+                    deauthenticationService.deauthenticate()
+                    keychain.appleAuthorizationUserId = nil
+                case .authorized, .notFound:
+                    break
+                @unknown default:
+                    break
+                }
+            }
+        }
+
+        authorizationCredentialRevocationObserving.revocationHandler = {
+            deauthenticationService.deauthenticate()
+            keychain.appleAuthorizationUserId = nil
+        }
+    }
+}
