076586b7372126ef0837f8d29fa8d8f669910058
davdroman <d@vidroman.dev>
2020-02-18 01:02:50 +0000

    Implement `Instrument` model and `InstrumentProvider` service

---
+++ Rythmico/InstrumentProviderDoubles.swift
@@ -0,0 +1,27 @@
+import Foundation
+import Sugar
+import struct SwiftUI.Image
+
+final class InstrumentProviderFake: InstrumentProviderProtocol {
+    func instruments(completion: Handler<[Instrument]>) {
+        let instruments = [
+            Instrument(id: "GUITAR", name: "Guitar", icon: Image(decorative: "instrument-icon-guitar")),
+            Instrument(id: "DRUMS", name: "Drums", icon: Image(decorative: "instrument-icon-guitar")),
+            Instrument(id: "PIANO", name: "Piano", icon: Image(decorative: "instrument-icon-guitar")),
+            Instrument(id: "SINGING", name: "Singing", icon: Image(decorative: "instrument-icon-guitar"))
+        ]
+        completion(instruments)
+    }
+}
+
+final class InstrumentProviderStub: InstrumentProviderProtocol {
+    var instruments: [Instrument]
+
+    init(instruments: [Instrument]) {
+        self.instruments = instruments
+    }
+
+    func instruments(completion: Handler<[Instrument]>) {
+        completion(instruments)
+    }
+}
