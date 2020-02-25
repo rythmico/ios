130f0acac1d381f50b851636ed6ccadb77609142
davdroman <d@vidroman.dev>
2020-02-25 18:42:53 +0000

    Tweak fonts and colors

--- UI/Colors.swift
+++ UI/Colors.swift
@@ -1,73 +1,51 @@
 import SwiftUI
 
 extension Color {
-    static var rythmicoRed: Color {
-        Color(base256Red: 176, green: 0, blue: 32)
-    }
+    static let rythmicoRed = Color(base256Red: 176, green: 0, blue: 32)
 
-    static var rythmicoPurple: Color {
-        Color(
-            lightModeVariant: .init(base256Red: 93, green: 97, blue: 241),
-            darkModeVariant: .init(base256Red: 83, green: 87, blue: 216)
-        )
-    }
+    static let rythmicoPurple = Color(.rythmicoPurple)
 
-    static var rythmicoGray90: Color {
-        Color(
-            lightModeVariant: .init(base256Red: 75, green: 93, blue: 107),
-            darkModeVariant: .init(base256Red: 165, green: 174, blue: 181)
-        )
-    }
+    static let rythmicoGray90 = Color(
+        lightModeVariant: .init(base256Red: 75, green: 93, blue: 107),
+        darkModeVariant: .init(base256Red: 165, green: 174, blue: 181)
+    )
 
-    static var rythmicoGray10: Color {
-        Color(
-            lightModeVariant: rythmicoGray10LightModeVariant,
-            darkModeVariant: .init(base256Red: 48, green: 48, blue: 49)
-        )
-    }
+    static let rythmicoGray20 = Color(.rythmicoGray20)
 
-    static var rythmicoWhite: Color {
-        Color(lightModeVariant: .white, darkModeVariant: rythmicoGray10LightModeVariant)
-    }
+    static let rythmicoGray10 = Color(
+        lightModeVariant: rythmicoGray10LightModeVariant,
+        darkModeVariant: .init(base256Red: 48, green: 48, blue: 49)
+    )
 
-    private static var rythmicoGray10LightModeVariant: UIColor {
-        UIColor(base256Red: 240, green: 244, blue: 247)
-    }
+    static let rythmicoWhite = Color(lightModeVariant: .white, darkModeVariant: rythmicoGray10LightModeVariant)
 
-    static var rythmicoForeground: Color {
-        Color(lightModeVariant: .black, darkModeVariant: .white)
-    }
+    private static let rythmicoGray10LightModeVariant = UIColor(base256Red: 240, green: 244, blue: 247)
 
-    static var rythmicoBackground: Color {
-        Color(lightModeVariant: .white, darkModeVariant: .black)
-    }
+    static let rythmicoForeground = Color(lightModeVariant: .black, darkModeVariant: .white)
+
+    static let rythmicoBackground = Color(lightModeVariant: .white, darkModeVariant: .black)
+
+    static let systemLightGray = Color(
+        lightModeVariant: .init(base256Red: 246, green: 246, blue: 246),
+        darkModeVariant: .init(base256Red: 40, green: 40, blue: 41)
+    )
 }
 
-private extension UIColor {
-    convenience init(
-        base256Red red: Int,
-        green: Int,
-        blue: Int,
-        alpha: CGFloat = 1
-    ) {
-        self.init(
-            red: CGFloat(red) / 255,
-            green: CGFloat(green) / 255,
-            blue: CGFloat(blue) / 255,
-            alpha: alpha
-        )
-    }
+extension UIColor {
+    static let rythmicoPurple = UIColor(
+        lightModeVariant: .init(base256Red: 93, green: 97, blue: 241),
+        darkModeVariant: .init(base256Red: 83, green: 87, blue: 216)
+    )
+
+    static let rythmicoGray20 = UIColor(
+        lightModeVariant: .init(base256Red: 195, green: 196, blue: 197),
+        darkModeVariant: .init(base256Red: 90, green: 90, blue: 94)
+    )
 }
 
 private extension Color {
     init(lightModeVariant: UIColor, darkModeVariant: UIColor) {
-        self.init(UIColor(dynamicProvider: {
-            if $0.userInterfaceStyle == .light {
-                return lightModeVariant
-            } else {
-                return darkModeVariant
-            }
-        }))
+        self.init(UIColor(lightModeVariant: lightModeVariant, darkModeVariant: darkModeVariant))
     }
 
     init(
@@ -83,6 +61,32 @@ private extension Color {
             green: Double(green) / 255,
             blue: Double(blue) / 255,
             opacity: opacity
+        )
+    }
+}
+
+private extension UIColor {
+    convenience init(lightModeVariant: UIColor, darkModeVariant: UIColor) {
+        self.init(dynamicProvider: {
+            if $0.userInterfaceStyle == .light {
+                return lightModeVariant
+            } else {
+                return darkModeVariant
+            }
+        })
+    }
+
+    convenience init(
+        base256Red red: Int,
+        green: Int,
+        blue: Int,
+        alpha: CGFloat = 1
+    ) {
+        self.init(
+            red: CGFloat(red) / 255,
+            green: CGFloat(green) / 255,
+            blue: CGFloat(blue) / 255,
+            alpha: alpha
         )
     }
 }
