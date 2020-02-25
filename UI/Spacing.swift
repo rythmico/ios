0f027fdb1079a4949f6ba7437f42167567f1d9b3
davdroman <d@vidroman.dev>
2020-02-25 19:00:22 +0000

    Add spacing constants

---
+++ UI/Spacing.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+extension CGFloat {
+    static var spacingUnit: CGFloat = 4
+    static var spacingExtraSmall: CGFloat = spacingUnit * 3
+    static var spacingSmall: CGFloat = spacingUnit * 4
+    static var spacingMedium: CGFloat = spacingUnit * 5
+    static var spacingLarge: CGFloat = spacingUnit * 6
+    static var spacingExtraLarge: CGFloat = spacingUnit * 7
+}
