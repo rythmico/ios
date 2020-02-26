4fbdf68734ebf3973364eec9ed375573376f2282
davdroman <d@vidroman.dev>
2020-02-26 06:01:53 +0000

    Implement `FloatingButton`

---
+++ UI/FloatingButton.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+import Sugar
+
+struct FloatingButton: View {
+    var title: String
+    var action: Action
+
+    var body: some View {
+        VStack(spacing: 0) {
+            Divider()
+            Button(action: action) {
+                HStack {
+                    Spacer()
+                    Text(title).rythmicoFont(.callout).foregroundColor(.rythmicoWhite)
+                    Spacer()
+                }
+                .padding(.vertical, .spacingSmall)
+                .frame(minHeight: 44)
+                .background(Color.rythmicoPurple.cornerRadius(4))
+            }
+            .padding(.vertical, .spacingExtraSmall)
+            .padding(.horizontal, .spacingMedium)
+        }
+        .background(Color.rythmicoBackground.edgesIgnoringSafeArea(.bottom))
+        .transition(.move(edge: .bottom))
+    }
+}
