7397cfc596fb064481678b62c93bf87d91948868
davdroman <d@vidroman.dev>
2020-02-29 19:15:13 +0000

    Gardening

--- UI/TitleContentView.swift
+++ UI/TitleContentView.swift
@@ -2,22 +2,21 @@ import SwiftUI
 
 struct TitleContentView<Content: View>: View {
     var title: [MultiStyleText.Part]
-    var content: () -> Content
+    var content: Content
 
-    init(title: String, @ViewBuilder content: @escaping () -> Content) {
-        self.title = [.init(title)]
-        self.content = content
+    init(title: [MultiStyleText.Part], @ViewBuilder content: () -> Content) {
+        self.title = title
+        self.content = content()
     }
 
-    init(title: [MultiStyleText.Part], @ViewBuilder content: @escaping () -> Content) {
-        self.title = title
-        self.content = content
+    init(title: String, @ViewBuilder content: () -> Content) {
+        self.init(title: [.init(title)], content: content)
     }
 
     var body: some View {
         VStack(alignment: .leading, spacing: .spacingExtraSmall) {
             MultiStyleText(style: .callout, parts: title)
-            content()
+            content
         }
     }
 }
--- UI/TitleSubtitleContentView.swift
+++ UI/TitleSubtitleContentView.swift
@@ -3,22 +3,22 @@ import SwiftUI
 struct TitleSubtitleContentView<Content: View>: View {
     var title: String
     var subtitle: [MultiStyleText.Part]
-    var content: () -> Content
+    var content: Content
 
-    init(title: String, subtitle: [MultiStyleText.Part], @ViewBuilder content: @escaping () -> Content) {
+    init(title: String, subtitle: [MultiStyleText.Part], @ViewBuilder content: () -> Content) {
         self.title = title
         self.subtitle = subtitle
-        self.content = content
+        self.content = content()
     }
 
-    init(title: String, subtitle: String, @ViewBuilder content: @escaping () -> Content) {
+    init(title: String, subtitle: String, @ViewBuilder content: () -> Content) {
         self.init(title: title, subtitle: [.init(subtitle)], content: content)
     }
 
     var body: some View {
         VStack(spacing: .spacingExtraLarge) {
             TitleSubtitleView(title: title, subtitle: subtitle)
-            content()
+            content
         }.padding(.horizontal, .spacingMedium)
     }
 }
