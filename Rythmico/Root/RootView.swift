ec92be4f747c52c031935cc3038a5f089e9c84ba
davdroman <d@vidroman.dev>
2020-03-03 22:16:59 +0000

    Reimplement and test root screen following VSM pattern

--- Rythmico/Root/RootView.swift
+++ Rythmico/Root/RootView.swift
@@ -1,37 +1,93 @@
 import SwiftUI
-import ViewModel
+import Sugar
 
-enum RootViewData {
-    case unauthenticated(OnboardingView)
-    case authenticated(MainTabView)
+extension RootView {
+    enum UserState {
+        case unauthenticated(OnboardingView)
+        case authenticated(MainTabView)
 
-    var onboardingView: OnboardingView? {
-        guard case .unauthenticated(let onboardingView) = self else {
-            return nil
+        var onboardingView: OnboardingView? {
+            guard case .unauthenticated(let onboardingView) = self else {
+                return nil
+            }
+            return onboardingView
         }
-        return onboardingView
-    }
 
-    var mainTabView: MainTabView? {
-        guard case .authenticated(let mainTabView) = self else {
-            return nil
+        var mainTabView: MainTabView? {
+            guard case .authenticated(let mainTabView) = self else {
+                return nil
+            }
+            return mainTabView
         }
-        return mainTabView
     }
 }
 
-struct RootView: View, ViewModelable {
-    @ObservedObject var viewModel: RootViewModel
+struct RootView: View, TestableView {
+    private let keychain: KeychainProtocol
+    private let onboardingViewModel: OnboardingViewModel
+    private let authorizationCredentialStateProvider: AppleAuthorizationCredentialStateProvider
+    private let authorizationCredentialRevocationObserving: AppleAuthorizationCredentialRevocationObserving
+    private let authenticationAccessTokenProviderObserving: AuthenticationAccessTokenProviderObserving
+    private let deauthenticationService: DeauthenticationServiceProtocol
 
-    init(viewModel: RootViewModel) {
-        self.viewModel = viewModel
+    @State private(set) var state: UserState
+
+    init(
+        keychain: KeychainProtocol,
+        onboardingViewModel: OnboardingViewModel,
+        authorizationCredentialStateProvider: AppleAuthorizationCredentialStateProvider,
+        authorizationCredentialRevocationObserving: AppleAuthorizationCredentialRevocationObserving,
+        authenticationAccessTokenProviderObserving: AuthenticationAccessTokenProviderObserving,
+        deauthenticationService: DeauthenticationServiceProtocol
+    ) {
+        self.keychain = keychain
+        self.onboardingViewModel = onboardingViewModel
+        self.authorizationCredentialStateProvider = authorizationCredentialStateProvider
+        self.authorizationCredentialRevocationObserving = authorizationCredentialRevocationObserving
+        self.authenticationAccessTokenProviderObserving = authenticationAccessTokenProviderObserving
+        self.deauthenticationService = deauthenticationService
+
+        self._state = State(initialValue: .unauthenticated(OnboardingView(viewModel: onboardingViewModel)))
     }
 
+    var didAppear: Handler<Self>?
     var body: some View {
         ZStack {
-            viewData.onboardingView.zIndex(1).transition(AnyTransition.move(edge: .leading))
-            viewData.mainTabView.zIndex(2).transition(AnyTransition.move(edge: .trailing))
+            state.onboardingView.zIndex(1).transition(.move(edge: .leading))
+            state.mainTabView.zIndex(2).transition(.move(edge: .trailing))
         }
-        .animation(.easeInOut(duration: .durationMedium), value: viewData.mainTabView != nil)
+        .animation(.easeInOut(duration: .durationMedium), value: state.mainTabView != nil)
+        .onAppear { self.didAppear?(self) }
+        .onAppear(perform: onAppear)
+    }
+
+    private func onAppear() {
+        authenticationAccessTokenProviderObserving.statusDidChangeHandler = { provider in
+            if let provider = provider {
+                self.state = .authenticated(MainTabView(accessTokenProvider: provider))
+            } else {
+                self.keychain.appleAuthorizationUserId = nil
+                self.state = .unauthenticated(OnboardingView(viewModel: self.onboardingViewModel))
+            }
+        }
+
+        if let authorizationUserId = keychain.appleAuthorizationUserId {
+            authorizationCredentialStateProvider.getCredentialState(forUserID: authorizationUserId) { state in
+                switch state {
+                case .revoked, .transferred:
+                    self.deauthenticationService.deauthenticate()
+                    self.keychain.appleAuthorizationUserId = nil
+                case .authorized, .notFound:
+                    break
+                @unknown default:
+                    break
+                }
+            }
+        }
+
+        authorizationCredentialRevocationObserving.revocationHandler = {
+            self.deauthenticationService.deauthenticate()
+            self.keychain.appleAuthorizationUserId = nil
+        }
     }
 }
