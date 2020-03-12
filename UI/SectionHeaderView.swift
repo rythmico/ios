e7921699bd1f460aace020f4d09b7b44a5bb45ee
davdroman <d@vidroman.dev>
2020-03-12 05:26:45 +0000

    Implement `SectionHeaderView`

---
+++ UI/SectionHeaderView.swift
@@ -0,0 +1,33 @@
+import SwiftUI
+
+struct SectionHeaderView: View {
+    var title: String
+
+    var body: some View {
+        HStack(alignment: .center, spacing: .spacingExtraSmall) {
+            Text(title.localizedUppercase)
+                .rythmicoFont(.footnote)
+                .foregroundColor(.rythmicoGray90)
+            VStack {
+                Divider().background(Color.rythmicoGray20)
+            }
+        }
+    }
+}
+
+struct SectionHeaderContentView<Content: View>: View {
+    var title: String
+    var content: Content
+
+    init(title: String, @ViewBuilder content: () -> Content) {
+        self.title = title
+        self.content = content()
+    }
+
+    var body: some View {
+        VStack(spacing: .spacingSmall) {
+            SectionHeaderView(title: title)
+            content
+        }
+    }
+}
