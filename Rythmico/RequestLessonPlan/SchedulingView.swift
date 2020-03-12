fedcd7b4be7efab8b46c74d05a5febe8b5daa337
davdroman <d@vidroman.dev>
2020-03-12 05:48:18 +0000

    Add `SchedulingView` placeholder

---
+++ Rythmico/RequestLessonPlan/SchedulingView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct SchedulingView: View {
+    var body: some View {
+        TitleSubtitleContentView(title: "Lesson Schedule", subtitle: []) {
+            VStack {
+                Spacer()
+                Text("Coming next!")
+                Spacer()
+            }
+        }
+    }
+}
