5552705da14473adcbc1003ccc72dc5b980e2ed5
davdroman <d@vidroman.dev>
2020-03-12 05:33:08 +0000

    Implement `AddressSearchService`

--- Rythmico/RequestLessonPlan/AddressProvider.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-import Sugar
-
-protocol AddressProviderProtocol: AnyObject {
-    typealias CompletionHandler = SimpleResultHandler<[Address]>
-    func addresses(withPostcode postcode: String, completion: @escaping CompletionHandler)
-}
