32665290795a2e1470906be68af7dd83ac92c740
davdroman <d@vidroman.dev>
2020-03-12 05:34:21 +0000

    Implement Address Details screen

---
+++ Rythmico/RequestLessonPlan/AddressDetails/AddressSelectionView.swift
@@ -0,0 +1,40 @@
+import SwiftUI
+
+struct AddressSelectionView: View {
+    var addresses: [Address]
+    @Binding var selection: Address?
+
+    var body: some View {
+        VStack(spacing: .spacingExtraSmall) {
+            ForEach(addresses) { address in
+                HStack {
+                    Text(address.singleLineFormattedString)
+                        .rythmicoFont(self.textStyle(for: address))
+                        .foregroundColor(self.textColor(for: address))
+                        .animation(.none)
+                    Spacer()
+                }
+                .modifier(
+                    self.containerModifier(for: address)
+                        .animation(.easeInOut(duration: .durationShort))
+                )
+                .contentShape(Rectangle())
+                .onTapGesture { self.selection = address }
+            }
+        }
+    }
+
+    private func textColor(for address: Address) -> Color {
+        selection == address ? .accentColor : .rythmicoGray90
+    }
+
+    private func textStyle(for address: Address) -> Font.TextStyle {
+        selection == address ? .callout : .body
+    }
+
+    private func containerModifier(for address: Address) -> RoundedThickOutlineContainer {
+        selection == address
+            ? RoundedThickOutlineContainer(backgroundColor: .accentColor, borderColor: .accentColor)
+            : RoundedThickOutlineContainer()
+    }
+}
