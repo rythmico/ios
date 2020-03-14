28bb023110c29ab524beee2ee80687b922124e95
davdroman <d@vidroman.dev>
2020-03-14 15:51:13 +0000

    Unit test Request Lesson Plan flow

--- Rythmico/RequestLessonPlan/RequestLessonPlanViewTests.swift
+++ Rythmico/RequestLessonPlan/RequestLessonPlanViewTests.swift
@@ -3,11 +3,67 @@ import XCTest
 import struct SwiftUI.Image
 
 final class RequestLessonPlanViewTests: XCTestCase {
-    func testInstrumentSelectionIsFirstView() {
-        let instrumentProvider = InstrumentSelectionListProviderStub(instruments: [.pianoStub])
-        let view = RequestLessonPlanView(instrumentProvider: instrumentProvider)
-        XCTAssertNotNil(view.instrumentSelectionView)
+    var requestLessonPlanView: (RequestLessonPlanContext, RequestLessonPlanView) {
+        let context = RequestLessonPlanContext()
+        let instrumentProvider = InstrumentSelectionListProviderStub(instruments: [.guitarStub, .pianoStub])
+        let view = RequestLessonPlanView(context: context, instrumentProvider: instrumentProvider)
+        return (context, view)
     }
 
-    // TODO: complete coverage
+    func testInitialValues() {
+        let (_, view) = requestLessonPlanView
+
+        XCTAssertView(view) { view in
+            XCTAssertFalse(view.shouldShowBackButton)
+            XCTAssertEqual(view.currentStepNumber, 1)
+            XCTAssertEqual(view.stepCount, 6)
+            XCTAssertNotNil(view.instrumentSelectionView)
+        }
+    }
+
+    func testStudentDetailsPresentation() {
+        let (context, view) = requestLessonPlanView
+
+        XCTAssertView(view) { view in
+            context.instrument = .guitarStub
+
+            XCTAssertTrue(view.shouldShowBackButton)
+            XCTAssertEqual(view.currentStepNumber, 2)
+            XCTAssertEqual(view.stepCount, 6)
+            XCTAssertNotNil(view.studentDetailsView)
+        }
+    }
+
+    func testAddressDetailsPresentation() {
+        let (context, view) = requestLessonPlanView
+
+        XCTAssertView(view) { view in
+            context.instrument = .guitarStub
+            context.student = .davidStub
+
+            XCTAssertTrue(view.shouldShowBackButton)
+            XCTAssertEqual(view.currentStepNumber, 3)
+            XCTAssertEqual(view.stepCount, 6)
+            XCTAssertNotNil(view.addressDetailsView)
+        }
+    }
+
+    func testSchedulingViewPresentation() {
+        let (context, view) = requestLessonPlanView
+
+        XCTAssertView(view) { view in
+            context.instrument = .guitarStub
+            context.student = .davidStub
+            context.address = .stub
+
+            XCTAssertTrue(view.shouldShowBackButton)
+            XCTAssertEqual(view.currentStepNumber, 4)
+            XCTAssertEqual(view.stepCount, 6)
+            XCTAssertNotNil(view.schedulingView)
+        }
+    }
+
+    // TODO: testPrivateNoteViewPresentation
+    // TODO: testReviewProposalViewPresentation
+    // TODO: testRequestConfirmationViewPresentation
 }
