2769c0c64f7231956694e2eea025582fc30eae1d
davdroman <d@vidroman.dev>
2020-03-14 15:08:02 +0000

    Change `Address.id` property to `String` type

--- Rythmico/RequestLessonPlan/Address.swift
+++ Rythmico/RequestLessonPlan/Address.swift
@@ -1,7 +1,8 @@
 import Foundation
 import Sugar
 
-struct Address: Equatable {
+struct Address: Equatable, Identifiable {
+    var id: String
     var latitude: Double
     var longitude: Double
     var line1: String
@@ -11,21 +12,43 @@ struct Address: Equatable {
     var city: String
     var postcode: String
     var country: String
-}
 
-extension Address: Identifiable {
-    var id: Int {
-        var hasher = Hasher()
-        hasher.combine(latitude)
-        hasher.combine(longitude)
-        hasher.combine(line1)
-        hasher.combine(line2)
-        hasher.combine(line3)
-        hasher.combine(line4)
-        hasher.combine(city)
-        hasher.combine(postcode)
-        hasher.combine(country)
-        return hasher.finalize()
+    init(
+        id: String? = nil,
+        latitude: Double,
+        longitude: Double,
+        line1: String,
+        line2: String,
+        line3: String,
+        line4: String,
+        city: String,
+        postcode: String,
+        country: String
+    ) {
+        if let id = id {
+            self.id = id
+        } else {
+            var hasher = Hasher()
+            hasher.combine(latitude)
+            hasher.combine(longitude)
+            hasher.combine(line1)
+            hasher.combine(line2)
+            hasher.combine(line3)
+            hasher.combine(line4)
+            hasher.combine(city)
+            hasher.combine(postcode)
+            hasher.combine(country)
+            self.id = String(hasher.finalize())
+        }
+        self.latitude = latitude
+        self.longitude = longitude
+        self.line1 = line1
+        self.line2 = line2
+        self.line3 = line3
+        self.line4 = line4
+        self.city = city
+        self.postcode = postcode
+        self.country = country
     }
 }
 
