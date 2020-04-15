ba4d2efd5f69bd15d97283006ce8bb1003822cd2
davdroman <d@vidroman.dev>
2020-04-15 19:08:12 +0100

    Refactor `FloatingPicker` to use a `formatter` closure instead of `PickableOption`

--- UI/FloatingPicker.swift
+++ UI/FloatingPicker.swift
@@ -1,27 +1,27 @@
 import SwiftUI
 
-protocol PickableOption: Hashable {
-    var optionTitle: String { get }
-}
-
 // TODO: generalize components
 // - Implement FloatingView<Content: View> (white background)
+// - Implement FloatingViewContainer<Content: View> and helper View.floatingViewContainer function.
 // - Implement FloatingInputView<Content: View> (gray background)
 // - FloatingButton -> FloatingView<Button> (make button styling a modifier)
 // - FloatingDatePickerPicker -> FloatingInputView<DatePicker>
 // - FloatingPicker -> FloatingInputView<BetterPicker> (extract FloatingPicker's picker into BetterPicker with existing generics)
-struct FloatingPicker<Options: RandomAccessCollection, Selection: PickableOption>: View where Options.Element == Selection {
+struct FloatingPicker<Options: RandomAccessCollection, Selection: Hashable>: View where Options.Element == Selection {
     var options: Options
     var selection: Binding<Selection>
+    var formatter: (Selection) -> String
     var doneButtonAction: () -> Void
 
     init(
         options: Options,
         selection: Binding<Selection>,
+        formatter: @escaping (Selection) -> String,
         doneButtonAction: @escaping () -> Void
     ) {
         self.options = options
         self.selection = selection
+        self.formatter = formatter
         self.doneButtonAction = doneButtonAction
     }
 
@@ -38,7 +38,7 @@ struct FloatingPicker<Options: RandomAccessCollection, Selection: PickableOption
             }
             Picker("", selection: selection) {
                 ForEach(options, id: \.hashValue) {
-                    Text($0.optionTitle).tag($0)
+                    Text(self.formatter($0)).tag($0)
                 }
             }
             .labelsHidden()
@@ -51,9 +51,16 @@ struct FloatingPicker<Options: RandomAccessCollection, Selection: PickableOption
 }
 
 extension FloatingPicker where Selection: CaseIterable, Selection.AllCases == Options {
-    init(selection: Binding<Selection>, doneButtonAction: @escaping () -> Void) {
-        self.options = Selection.allCases
-        self.selection = selection
-        self.doneButtonAction = doneButtonAction
+    init(
+        selection: Binding<Selection>,
+        formatter: @escaping (Selection) -> String,
+        doneButtonAction: @escaping () -> Void
+    ) {
+        self.init(
+            options: Selection.allCases,
+            selection: selection,
+            formatter: formatter,
+            doneButtonAction: doneButtonAction
+        )
     }
 }
