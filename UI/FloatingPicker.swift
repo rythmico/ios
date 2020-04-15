e7a7f306bb5fb96f0213a111fc8e8647f8a6e957
davdroman <d@vidroman.dev>
2020-04-15 04:25:15 +0100

    Improve date and time picking

--- UI/FloatingPicker.swift
+++ UI/FloatingPicker.swift
@@ -1,12 +1,29 @@
 import SwiftUI
 
-protocol PickableOption: CaseIterable, RawRepresentable, Hashable where AllCases: RandomAccessCollection, RawValue: Hashable {
-    var title: String { get }
+protocol PickableOption: Hashable {
+    var optionTitle: String { get }
 }
 
-struct FloatingPicker<Selection: PickableOption>: View {
+// TODO: generalize components
+// - Implement FloatingView<Content: View> (white background)
+// - Implement FloatingInputView<Content: View> (gray background)
+// - FloatingButton -> FloatingView<Button> (make button styling a modifier)
+// - FloatingDatePickerPicker -> FloatingInputView<DatePicker>
+// - FloatingPicker -> FloatingInputView<Picker> (extract FloatingPicker generics and apply to Picker alone)
+struct FloatingPicker<Options: RandomAccessCollection, Selection: PickableOption>: View where Options.Element == Selection {
+    var options: Options
     var selection: Binding<Selection>
     var doneButtonAction: () -> Void
+
+    init(
+        options: Options,
+        selection: Binding<Selection>,
+        doneButtonAction: @escaping () -> Void
+    ) {
+        self.options = options
+        self.selection = selection
+        self.doneButtonAction = doneButtonAction
+    }
 
     var body: some View {
         VStack(alignment: .center, spacing: 0) {
@@ -20,8 +37,8 @@ struct FloatingPicker<Selection: PickableOption>: View {
                 .padding(.top, .spacingExtraSmall)
             }
             Picker("", selection: selection) {
-                ForEach(Selection.allCases, id: \.rawValue) {
-                    Text($0.title).tag($0)
+                ForEach(options, id: \.hashValue) {
+                    Text($0.optionTitle).tag($0)
                 }
             }
             .labelsHidden()
@@ -30,5 +47,13 @@ struct FloatingPicker<Selection: PickableOption>: View {
         .background(Color.systemLightGray.edgesIgnoringSafeArea(.bottom))
         .transition(.move(edge: .bottom))
         .onAppear(perform: UIApplication.shared.dismissKeyboard)
+    }
+}
+
+extension FloatingPicker where Selection: CaseIterable, Selection.AllCases == Options {
+    init(selection: Binding<Selection>, doneButtonAction: @escaping () -> Void) {
+        self.options = Selection.allCases
+        self.selection = selection
+        self.doneButtonAction = doneButtonAction
     }
 }
