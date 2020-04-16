b6e51619627647a14baf10975ec2092ad221e26b
davdroman <d@vidroman.dev>
2020-04-17 00:48:37 +0100

    Refactor

---
+++ UI/Transitions.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+// TODO: remove when AnyTransition.move(edge:) respects edgesIgnoringSafeArea.
+extension AnyTransition {
+    static func move(edgeWithSafeArea edge: Edge) -> AnyTransition {
+        move(edge: .bottom).combined(
+            with: .offset(
+                UIApplication.shared.windows[0].safeAreaInsets.offset(for: edge)
+            )
+        )
+    }
+}
+
+private extension UIEdgeInsets {
+    func offset(for edge: Edge) -> CGSize {
+        switch edge {
+        case .top:
+            return .init(width: 0, height: top)
+        case .leading:
+            return .init(width: left, height: 0)
+        case .bottom:
+            return .init(width: 0, height: bottom)
+        case .trailing:
+            return .init(width: right, height: 0)
+        }
+    }
+}
