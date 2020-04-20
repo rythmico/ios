168736c14499ad307a87957957dbc39edfbca06b
davdroman <d@vidroman.dev>
2020-04-20 22:25:00 +0100

    Refactor

---
+++ Rythmico/RequestLessonPlan/Address/AddressItemView.swift
@@ -0,0 +1,31 @@
+import SwiftUI
+
+struct AddressItemView: View {
+    var title: String
+    var isSelected: Bool
+
+    var body: some View {
+        Text(title)
+            .rythmicoFont(textStyle)
+            .foregroundColor(textColor)
+            .minimumScaleFactor(0.9)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .animation(.none)
+            .modifier(containerModifier.animation(.rythmicoSpring(duration: .durationShort)))
+            .contentShape(Rectangle())
+    }
+
+    private var textColor: Color {
+        isSelected ? .accentColor : .rythmicoGray90
+    }
+
+    private var textStyle: Font.TextStyle {
+        isSelected ? .callout : .body
+    }
+
+    private var containerModifier: RoundedThickOutlineContainer {
+        isSelected
+            ? RoundedThickOutlineContainer(backgroundColor: .accentColor, borderColor: .accentColor)
+            : RoundedThickOutlineContainer()
+    }
+}
