5e1764c542ac1146b91ebb89202a2ddd35b0c0df
davdroman <d@vidroman.dev>
2020-04-20 22:26:15 +0100

    Add more student doubles

--- Rythmico/RequestLessonPlan/Student/StudentDoubles.swift
+++ Rythmico/RequestLessonPlan/Student/StudentDoubles.swift
@@ -4,9 +4,33 @@ extension Student {
     static var davidStub: Student {
         .init(
             name: "David Roman",
-            dateOfBirth: Date(),
+            dateOfBirth: Date().adding(-9, .year),
+            gender: .male,
+            about: """
+            Something qwdsqw sqw qwdsqwsq
+            Swdqwd qwd swqs qws qw dq wd
+            """
+        )
+    }
+
+    static var davidStubNoAbout: Student {
+        .init(
+            name: "David Roman",
+            dateOfBirth: Date().adding(-9, .year),
             gender: .male,
             about: ""
+        )
+    }
+
+    static var unnamedStub: Student {
+        .init(
+            name: "",
+            dateOfBirth: Date().adding(-9, .year),
+            gender: .male,
+            about: """
+            Something qwdsqw sqw qwdsqwsq
+            Swdqwd qwd swqs qws qw dq wd
+            """
         )
     }
 }
