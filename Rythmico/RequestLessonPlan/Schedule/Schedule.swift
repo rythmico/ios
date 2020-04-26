44a5764385cc294d3393d933c58999924c0e2901
davdroman <d@vidroman.dev>
2020-04-26 15:56:44 +0100

    Add `CasePickable` protocol

--- Rythmico/RequestLessonPlan/Schedule/Schedule.swift
+++ Rythmico/RequestLessonPlan/Schedule/Schedule.swift
@@ -5,7 +5,11 @@ struct Schedule: Equatable {
     var duration: Duration
 }
 
-enum Duration: Int, Equatable, CaseIterable {
+enum Duration: Int, Equatable, CasePickable, CaseIterable {
+    // NOTE: do not EVER remove any existing cases.
+    // To modify picker options, instead explicitly specify
+    // which cases you want to show on a picker by declaring
+    // `static var pickableCases: [Duration] { [...] }`
     case fortyFiveMinutes = 45
     case oneHour = 60
     case oneHourThirtyMinutes = 90
