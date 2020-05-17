2ef116064d64d4b6cc6c0b7092bee893f382b92a
davdroman <d@vidroman.dev>
2020-05-17 17:50:06 +0100

    Implement secondary button style

--- UI/Buttons.swift
+++ UI/Buttons.swift
@@ -5,6 +5,10 @@ extension Button {
     func primaryStyle() -> some View {
         buttonStyle(PrimaryButtonStyle())
     }
+
+    func secondaryStyle() -> some View {
+        buttonStyle(SecondaryButtonStyle())
+    }
 }
 
 struct PrimaryButtonStyle: ButtonStyle {
@@ -20,5 +24,33 @@ struct PrimaryButtonStyle: ButtonStyle {
         configuration.isPressed
             ? Color.rythmicoHighlightPurple
             : Color.rythmicoPurple
+    }
+}
+
+struct SecondaryButtonStyle: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .rythmicoFont(.callout)
+            .foregroundColor(foregroundColor(for: configuration))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .overlay(
+                RoundedRectangle(cornerRadius: 4)
+                    .stroke(Color.rythmicoGray30, lineWidth: 2)
+            )
+            .background(
+                backgroundColor(for: configuration).cornerRadius(4)
+            )
+    }
+
+    func foregroundColor(for configuration: Configuration) -> Color {
+        configuration.isPressed
+            ? Color.rythmicoWhite
+            : Color.rythmicoGray90
+    }
+
+    func backgroundColor(for configuration: Configuration) -> Color {
+        configuration.isPressed
+            ? Color.rythmicoGray30
+            : Color.clear
     }
 }
