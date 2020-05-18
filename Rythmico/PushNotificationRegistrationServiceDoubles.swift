f00c56aa5e46d4929055676b984f3bc1c2836150
davdroman <d@vidroman.dev>
2020-05-18 19:00:05 +0100

    Unit test push notification registration

--- Rythmico/PushNotificationRegistrationServiceDoubles.swift
+++ Rythmico/PushNotificationRegistrationServiceDoubles.swift
@@ -1,8 +1,24 @@
 import Foundation
 
+final class PushNotificationRegistrationServiceSpy: PushNotificationRegistrationServiceProtocol {
+    var registerCount = 0
+
+    func registerForPushNotifications() {
+        registerCount += 1
+    }
+}
+
 final class PushNotificationRegistrationServiceDummy: PushNotificationRegistrationServiceProtocol {
     func registerForPushNotifications() {
         // NO-OP
+    }
+}
+
+final class PushNotificationUnregistrationServiceSpy: PushNotificationUnregistrationServiceProtocol {
+    var unregisterCount = 0
+
+    func unregisterForPushNotifications() {
+        unregisterCount += 1
     }
 }
 
