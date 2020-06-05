1a298aea7cba235f49c0cefe3969bd8cf03d68b5
davdroman <d@vidroman.dev>
2020-06-05 13:50:32 +0100

    Implement `LessonPlanSummaryCell`

---
+++ Rythmico/Lessons/LessonPlanSummaryCell.swift
@@ -0,0 +1,94 @@
+import SwiftUI
+
+struct LessonPlanSummaryCell: View {
+    var lessonPlan: LessonPlan
+    var transitionDelay: Double?
+
+    var title: String {
+        [
+            lessonPlan.student.name.firstWord,
+            "\(lessonPlan.instrument.name) Lessons"
+        ].compactMap { $0 }.joined(separator: " - ")
+    }
+
+    var subtitle: String {
+        switch lessonPlan.status {
+        case .pending:
+            return "Pending tutor applications"
+        default:
+            return "" // TODO
+        }
+    }
+
+    var tutorImage: AnyView {
+        switch lessonPlan.status {
+        case .pending:
+            return AnyView(Image(systemSymbol: .person).foregroundColor(.rythmicoGray90).font(.system(size: 18, weight: .medium, design: .rounded)))
+        default:
+            return AnyView(Image(systemSymbol: .personFill)) // TODO
+        }
+    }
+
+    var tutor: String {
+        switch lessonPlan.status {
+        case .pending:
+            return "Tutor TBC"
+        default:
+            return "" // TODO
+        }
+    }
+
+    var status: String {
+        switch lessonPlan.status {
+        case .pending:
+            return "Pending"
+        default:
+            return "" // TODO
+        }
+    }
+
+    var body: some View {
+        HStack(spacing: 0) {
+            VStack(alignment: .leading, spacing: .spacingExtraSmall) {
+                Text(title)
+                    .rythmicoFont(.headline)
+                    .foregroundColor(.rythmicoForeground)
+                Text(subtitle)
+                    .rythmicoFont(.body)
+                    .foregroundColor(.rythmicoGray90)
+                HStack(spacing: .spacingExtraSmall) {
+                    tutorImage
+                        .frame(width: 32, height: 32)
+                        .background(Color.rythmicoGray10.clipShape(Circle()))
+                    Group {
+                        Text(tutor)
+                            .lineLimit(2)
+                            .foregroundColor(.rythmicoGray90)
+                            .frame(maxWidth: .infinity, alignment: .leading)
+                        Text(status)
+                            .lineLimit(1)
+                            .foregroundColor(.rythmicoDarkPurple)
+                            .padding(.vertical, .spacingUnit / 2)
+                            .padding(.horizontal, .spacingExtraSmall)
+                            .frame(minWidth: 96)
+                            .background(Color.rythmicoLightPurple.clipShape(Capsule()))
+                    }
+                    .rythmicoFont(.body)
+                    .minimumScaleFactor(0.7)
+                }
+            }
+        }
+        .padding(.spacingMedium)
+        .modifier(RoundedShadowContainer())
+        .transition(
+            AnyTransition
+                .opacity
+                .combined(with: .scale(scale: 0.8))
+                .animation(
+                    Animation
+                        .rythmicoSpring(duration: .durationMedium)
+                        .delay(transitionDelay ?? 0)
+                )
+        )
+    }
+}
