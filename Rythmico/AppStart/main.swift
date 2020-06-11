487d021677423c37dce83fe1e1c7f84070398057
davdroman <d@vidroman.dev>
2020-06-11 18:14:05 +0100

    Gardening

--- Rythmico/AppStart/main.swift
+++ Rythmico/AppStart/main.swift
@@ -1,6 +1,7 @@
 import UIKit
 
 let isRunningTests = NSClassFromString("XCTestCase") != nil
+let isRunningPreviews = ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1"
 
 UIApplicationMain(
     CommandLine.argc,
