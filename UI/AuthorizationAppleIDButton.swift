281432d37af99cf723b8a66ae83cda0cbf6e6235
davdroman <d@vidroman.dev>
2020-06-12 23:10:03 +0100

    Make Continue with Apple button dynamic sized

--- UI/AuthorizationAppleIDButton.swift
+++ UI/AuthorizationAppleIDButton.swift
@@ -3,37 +3,42 @@ import AuthenticationServices
 
 struct AuthorizationAppleIDButton: View {
     private enum Const {
-        static let maxWidth: CGFloat = 350
-        static let height: CGFloat = 44
+        static let maxWidth: CGFloat = 320
+        static let defaultHeight: CGFloat = 44
     }
 
     @Environment(\.colorScheme) private var colorScheme
+    @Environment(\.sizeCategory) private var sizeCategory
+
+    var type: ASAuthorizationAppleIDButton.ButtonType = .continue
 
     var body: some View {
         Group {
             // Required to force SwiftUI to recreate the UIViewRepresentable
             // https://stackoverflow.com/a/56852456
             if colorScheme == .light {
-                Representable()
+                Representable(type: type)
             } else {
-                Representable()
+                Representable(type: type)
             }
         }
         .frame(maxWidth: Const.maxWidth)
-        .frame(height: Const.height)
+        .frame(height: Const.defaultHeight * sizeCategory.sizeFactor)
     }
 }
 
 extension AuthorizationAppleIDButton {
     private struct Representable: UIViewRepresentable {
+        var type: ASAuthorizationAppleIDButton.ButtonType
+
         func makeUIView(context: Context) -> ASAuthorizationAppleIDButton {
             switch context.environment.colorScheme {
             case .light:
-                return ASAuthorizationAppleIDButton(type: .continue, style: .black)
+                return ASAuthorizationAppleIDButton(type: type, style: .black)
             case .dark:
-                return ASAuthorizationAppleIDButton(type: .continue, style: .white)
+                return ASAuthorizationAppleIDButton(type: type, style: .white)
             @unknown default:
-                return ASAuthorizationAppleIDButton(type: .continue, style: .black)
+                return ASAuthorizationAppleIDButton(type: type, style: .black)
             }
         }
 
@@ -50,6 +55,8 @@ struct AuthorizationAppleIDButton_Preview: PreviewProvider {
                 ForEach(ColorScheme.allCases, id: \.self) {
                     AuthorizationAppleIDButton()
                         .environment(\.colorScheme, $0)
+                        .environment(\.sizeCategory, .extraSmall)
+//                        .environment(\.sizeCategory, .accessibilityExtraExtraExtraLarge)
                         .padding()
                 }
             }
