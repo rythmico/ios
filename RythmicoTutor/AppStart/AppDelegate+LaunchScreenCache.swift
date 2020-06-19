e6bccfa3f09ba5a6056d52b4e441a6e7661287e3
davdroman <d@vidroman.dev>
2020-06-19 14:53:48 +0100

    Set up app start delegates

---
+++ RythmicoTutor/AppStart/AppDelegate+LaunchScreenCache.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+extension AppDelegate {
+    func clearLaunchScreenCache(_ shouldClear: Bool) {
+        #if DEBUG
+        guard shouldClear else { return }
+
+        do {
+            try FileManager.default.removeItem(atPath: NSHomeDirectory() + "/Library/SplashBoard")
+        } catch {
+            print("Failed to delete launch screen cache: \(error)")
+        }
+        #endif
+    }
+}
---
+++ RythmicoTutor/AppStart/AppDelegate.swift
@@ -0,0 +1,18 @@
+import UIKit
+import Sugar
+
+final class AppDelegate: UIResponder, UIApplicationDelegate {
+    private enum Const {
+        static let launchScreenDebugMode = false
+        static let launchScreenDelay = launchScreenDebugMode ? 5 : .durationMedium
+    }
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        clearLaunchScreenCache(Const.launchScreenDebugMode)
+        sleep(Const.launchScreenDelay)
+        configureWindow()
+        return true
+    }
+}
