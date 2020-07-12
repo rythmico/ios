deccaf5cf16b9456b30d4fde40684a151a346048
davdroman <d@vidroman.dev>
2020-07-12 20:28:17 +0100

    Implement loading spinner

--- RythmicoTutor/BookingRequests/BookingRequestsView.swift
+++ RythmicoTutor/BookingRequests/BookingRequestsView.swift
@@ -1,40 +1,53 @@
 import SwiftUI
 
 struct BookingRequestsView: View {
-    private let coordinator = Current.bookingRequestFetchingCoordinator()!
+    @ObservedObject
+    private var coordinator = Current.bookingRequestFetchingCoordinator()!
     @ObservedObject
     private var repository = Current.bookingRequestRepository
 
     private let lessonDateFormatter = Current.dateFormatter(format: .custom("d MMM '@' HH:mm"))
     private let bookingDateFormatter = Current.relativeDateTimeFormatter(context: .standalone, style: .short)
 
-    var requests: [BookingRequest] { repository.latestBookingRequests }
+    var isLoading: Bool { coordinator.state.isLoading }
     var error: Error? { coordinator.state.failureValue }
+    var requests: [BookingRequest] { repository.latestBookingRequests }
 
     var body: some View {
-        List {
-            Section(header: Text("LATEST")) {
-                ForEach(requests) { request in
-                    HStack(spacing: .spacingSmall) {
-                        VStack(alignment: .leading) {
-                            Text(request.student.name)
-                                .foregroundColor(.primary)
-                                .font(.body)
-                            Text("\(request.schedule.startDate, formatter: self.lessonDateFormatter) • \(request.postcode)")
-                            .foregroundColor(.secondary)
-                            .font(.callout)
+        VStack(spacing: .spacingMedium) {
+            List {
+                Section(
+                    header: HStack {
+                        Text("LATEST")
+                        if isLoading {
+                            ActivityIndicator(style: .medium)
+                                .transition(AnyTransition.opacity.combined(with: .scale))
                         }
-                        Spacer(minLength: 0)
-                        Text("\(request.createdAt, formatter: self.bookingDateFormatter)")
-                            .foregroundColor(.secondary)
-                            .font(.caption)
-                            .fontWeight(.medium)
                     }
-                    .padding(.vertical, .spacingUnit)
+                ) {
+                    ForEach(requests) { request in
+                        HStack(spacing: .spacingSmall) {
+                            VStack(alignment: .leading) {
+                                Text(request.student.name)
+                                    .foregroundColor(.primary)
+                                    .font(.body)
+                                Text("\(request.schedule.startDate, formatter: self.lessonDateFormatter) • \(request.postcode)")
+                                .foregroundColor(.secondary)
+                                .font(.callout)
+                            }
+                            Spacer(minLength: 0)
+                            Text("\(request.createdAt, formatter: self.bookingDateFormatter)")
+                                .foregroundColor(.secondary)
+                                .font(.caption)
+                                .fontWeight(.medium)
+                        }
+                        .padding(.vertical, .spacingUnit)
+                    }
                 }
             }
+            .listStyle(GroupedListStyle())
         }
-        .listStyle(GroupedListStyle())
+        .animation(.rythmicoSpring(duration: .durationShort, type: .damping), value: isLoading)
         .alert(error: self.error, dismiss: coordinator.dismissFailure)
         .onAppear(perform: coordinator.fetchBookingRequests)
     }
