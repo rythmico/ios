5437f850bac603243a3c529582842909baf7aae5
David Roman <d@vidroman.dev>
2020-07-28 23:29:55 +0100

    Revert "Tech: Disable Bitcode"

--- UI/SheetInteractiveDismissal.swift
+++ UI/SheetInteractiveDismissal.swift
@@ -2,14 +2,9 @@ import SwiftUI
 import Introspect
 
 extension View {
-    func sheetInteractiveDismissal(_ enabled: Bool, onAttempt: (() -> Void)? = nil) -> some View {
+    func sheetInteractiveDismissal(_ enabled: Bool) -> some View {
         introspectViewController {
-            guard let presentationController = $0.ultimateParent.presentationController else {
-                return
-            }
-            let delegate = AdaptivePresentationControllerDelegate(shouldDismiss: enabled, didAttemptToDismiss: onAttempt)
-            controllerDelegateMap.setObject(delegate, forKey: presentationController)
-            presentationController.delegate = delegate
+            $0.ultimateParent.isModalInPresentation = !enabled
         }
     }
 }
@@ -17,23 +12,3 @@ extension View {
 private extension UIViewController {
     var ultimateParent: UIViewController { parent?.ultimateParent ?? self }
 }
-
-private final class AdaptivePresentationControllerDelegate: NSObject, UIAdaptivePresentationControllerDelegate {
-    let shouldDismiss: Bool
-    let didAttemptToDismiss: (() -> Void)?
-
-    init(shouldDismiss: Bool, didAttemptToDismiss: (() -> Void)?) {
-        self.shouldDismiss = shouldDismiss
-        self.didAttemptToDismiss = didAttemptToDismiss
-    }
-
-    func presentationControllerShouldDismiss(_ presentationController: UIPresentationController) -> Bool {
-        shouldDismiss
-    }
-
-    func presentationControllerDidAttemptToDismiss(_ presentationController: UIPresentationController) {
-        didAttemptToDismiss?()
-    }
-}
-
-private var controllerDelegateMap = NSMapTable<UIPresentationController, AdaptivePresentationControllerDelegate>(keyOptions: .weakMemory, valueOptions: .strongMemory)
