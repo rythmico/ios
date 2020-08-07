e165f5f715943413fa0393269e9647bfe846a189
davdroman <d@vidroman.dev>
2020-08-07 20:47:38 +0100

    Implement Tutor specific `FloatingView`

--- UI/FloatingView+Tutor.swift
+++ UI/FloatingView+Tutor.swift
@@ -1,8 +1,19 @@
 import SwiftUI
+import Sugar
 
-extension FloatingView {
+struct FloatingView<Content: View>: View {
+    var content: Content
+
     init(@ViewBuilder content: () -> Content) {
-        self.backgroundColor = .init(.systemBackground)
         self.content = content()
+    }
+
+    var body: some View {
+        VStack(spacing: 0) {
+            Divider()
+            content.padding(.spacingSmall)
+        }
+        .background(Color(.systemBackground).edgesIgnoringSafeArea(.bottom))
+        .transition(.move(edgeWithSafeArea: .bottom))
     }
 }
