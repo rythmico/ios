a85202d1baf5d8c5b08ed9960620549d2f180a2c
davdroman <d@vidroman.dev>
2020-08-11 18:27:37 +0100

    Refactor

--- Services/MapOpener/MapOpener+ActionSheet.swift
+++ Services/MapOpener/MapOpener+ActionSheet.swift
@@ -5,26 +5,34 @@ extension View {
     func mapOpeningSheet(
         isPresented: Binding<Bool>,
         mapOpener: MapOpenerProtocol = Current.mapOpener,
-        action: MapLink.Action,
+        intent: MapLink.Intent,
         error: Binding<Error?>
     ) -> some View {
-        actionSheet(isPresented: isPresented) {
-            ActionSheet(
-                title: Text("Open in..."),
-                message: nil,
-                buttons: [
-                    .default(Text("Apple Maps"), action: errorBindingAction(for: { try mapOpener.open(.appleMaps(action)) }, through: error)),
-                    .default(Text("Google Maps"), action: errorBindingAction(for: { try mapOpener.open(.googleMaps(action, zoom: 10)) }, through: error)),
-                    .cancel()
-                ]
-            )
+        let mapLinks: [MapLink] = [
+            .appleMaps(intent),
+            .googleMaps(intent, zoom: 10),
+        ]
+
+        func button(for link: MapLink) -> ActionSheet.Button {
+            .default(Text(link.appName)) {
+                error.wrappedValue = Result { try mapOpener.open(link) }.failureValue
+            }
+        }
+
+        return actionSheet(isPresented: isPresented) {
+            ActionSheet(title: Text("Open in..."), buttons: mapLinks.map(button) + .cancel())
         }
         .alert(error: error)
     }
+}
 
-    private func errorBindingAction(for action: @escaping () throws -> Void, through errorBinding: Binding<Error?>) -> (() -> Void) {
-        {
-            errorBinding.wrappedValue = Result(catching: action).failureValue
+private extension MapLink {
+    var appName: String {
+        switch self {
+        case .appleMaps:
+            return "Apple Maps"
+        case .googleMaps:
+            return "Google Maps"
         }
     }
 }
