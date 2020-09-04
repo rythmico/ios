f018de3bf50c367ff2400318966f5fc3452c053e
davdroman <d@vidroman.dev>
2020-09-04 01:07:06 +0100

    Fix tutor initials layout issues

--- UI/AvatarView.swift
+++ UI/AvatarView.swift
@@ -46,12 +46,12 @@ struct AvatarView: View {
         return AnyView(
             GeometryReader { g in
                 Text(initials)
-                    .font(.system(size: max(g.size.width, g.size.height), weight: .medium, design: .rounded))
+                    .font(.system(size: g.size.width, weight: .medium, design: .rounded))
                     .minimumScaleFactor(.leastNonzeroMagnitude)
                     .lineLimit(1)
                     .foregroundColor(.rythmicoGray90)
                     .padding(.horizontal, g.size.width * 0.18)
-                    .position(x: g.frame(in: .local).midX, y: g.frame(in: .local).midY)
+                    .position(x: g.size.width / 2, y: g.size.height / 2)
             }
         )
     }
@@ -81,7 +81,7 @@ struct AvatarView: View {
                     .foregroundColor(.rythmicoGray90)
                     .padding(.horizontal, g.size.width * 0.255)
                     .offset(x: 0, y: -g.size.height * 0.02)
-                    .position(x: g.frame(in: .local).midX, y: g.frame(in: .local).midY)
+                    .position(x: g.size.width / 2, y: g.size.height / 2)
             }
         )
     }
