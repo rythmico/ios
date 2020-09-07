ce2e423e00f5bd83af5cae115b21303a78525d6a
davdroman <d@vidroman.dev>
2020-09-07 17:38:46 +0100

    Handle LESSON_PLAN_UPDATE remote event

--- Services/PushNotifications/PushNotificationEventHandler+Rythmico.swift
+++ Services/PushNotifications/PushNotificationEventHandler+Rythmico.swift
@@ -1,7 +1,23 @@
 import Foundation
+import Combine
 
 final class PushNotificationEventHandler: PushNotificationEventHandlerProtocol {
+    private var cancellables: [AnyCancellable] = []
+
     func handle(_ event: PushNotificationEvent) {
-        // TODO
+        switch event {
+        case .lessonPlanUpdate: handleLessonPlanUpdateEvent()
+        }
+    }
+
+    private func handleLessonPlanUpdateEvent() {
+        guard let coordinator = Current.coordinator(for: \.lessonPlanFetchingService) else {
+            return
+        }
+        coordinator.$state
+            .compactMap(\.successValue)
+            .sink(receiveValue: Current.lessonPlanRepository.setItems)
+            .store(in: &cancellables)
+        coordinator.run()
     }
 }
