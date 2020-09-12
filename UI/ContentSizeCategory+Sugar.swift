5cfaa7020235864a53397fe934b7cf831da1f77c
davdroman <d@vidroman.dev>
2020-09-12 22:45:32 +0100

    Complete some TODOs

--- UI/ContentSizeCategory+Sugar.swift
+++ UI/ContentSizeCategory+Sugar.swift
@@ -1,30 +1,6 @@
 import SwiftUI
 
 extension ContentSizeCategory {
-    // TODO: remove after obsoleting iOS 13
-    var _isAccessibilityCategory: Bool {
-        switch self {
-        case .accessibilityMedium,
-             .accessibilityLarge,
-             .accessibilityExtraLarge,
-             .accessibilityExtraExtraLarge,
-             .accessibilityExtraExtraExtraLarge:
-            return true
-        case .extraSmall,
-             .small,
-             .medium,
-             .large,
-             .extraLarge,
-             .extraExtraLarge,
-             .extraExtraExtraLarge:
-            return false
-        @unknown default:
-            return false
-        }
-    }
-}
-
-extension ContentSizeCategory {
     var sizeFactor: CGFloat {
         switch self {
         case .extraSmall:
