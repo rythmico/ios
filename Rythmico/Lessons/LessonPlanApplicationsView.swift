1b80c646c3b45c84eab16a6309c6f00563ed8ceb
davdroman <d@vidroman.dev>
2020-09-16 02:19:08 +0100

    Open tutor portfolios on tap

--- Rythmico/Lessons/LessonPlanApplicationsView.swift
+++ Rythmico/Lessons/LessonPlanApplicationsView.swift
@@ -1,8 +1,8 @@
 import SwiftUI
 
 struct LessonPlanApplicationsView: View, RoutableView {
-    private enum Const {
-        static let avatarSize = .spacingUnit * 14
+    final class ViewState: ObservableObject {
+        @Published var selectedApplication: LessonPlan.Application?
     }
 
     @Environment(\.presentationMode)
@@ -10,6 +10,12 @@ struct LessonPlanApplicationsView: View, RoutableView {
 
     private var lessonPlan: LessonPlan
     private var applications: [LessonPlan.Application]
+    @StateObject
+    private var state = ViewState()
+    @State
+    private var shouldShowInfoBannerOnAppear = false
+    @State
+    private var isShowingInfoBanner = false
 
     init?(_ lessonPlan: LessonPlan) {
         guard let applications = lessonPlan.status.reviewingValue else {
@@ -39,26 +45,24 @@ struct LessonPlanApplicationsView: View, RoutableView {
     var body: some View {
         VStack(alignment: .leading, spacing: .spacingSmall) {
             TitleContentView(title: "Tutors Available") {
-                InfoBanner(text: priceInfo)
+                if isShowingInfoBanner {
+                    InfoBanner(text: priceInfo)
+                }
             }
             .padding(.horizontal, .spacingMedium)
 
             ScrollView(showsIndicators: false) {
                 LazyVGrid(columns: columns, alignment: .center, spacing: .spacingSmall) {
                     ForEach(applications, id: \.self) { application in
-                        VStack(spacing: .spacingExtraSmall) {
-                            LessonPlanTutorAvatarView(application.tutor, thumbnail: true)
-                                .frame(width: Const.avatarSize, height: Const.avatarSize)
-                            Text(application.tutor.name)
-                                .rythmicoFont(.bodyBold)
-                                .foregroundColor(.rythmicoForeground)
-                                .lineLimit(1)
-                                .minimumScaleFactor(0.6)
-                        }
-                        .padding(.vertical, .spacingExtraLarge)
-                        .padding(.horizontal, .spacingSmall)
-                        .frame(maxWidth: .infinity)
-                        .modifier(RoundedShadowContainer())
+                        NavigationLink(
+                            destination: LessonPlanApplicationDetailView(
+                                lessonPlan: lessonPlan,
+                                application: application
+                            ),
+                            tag: application,
+                            selection: $state.selectedApplication,
+                            label: { LessonPlanApplicationCell(application) }
+                        )
                     }
                 }
                 .padding(.top, .spacingUnit * 2)
@@ -68,10 +72,20 @@ struct LessonPlanApplicationsView: View, RoutableView {
         .padding(.top, .spacingExtraSmall)
         .navigationBarTitleDisplayMode(.inline)
         .routable(self)
+        .onReceive(state.$selectedApplication, perform: onSelectedApplicationChanged)
+        .onAppear(perform: onAppear)
     }
 
     private func back() {
         presentationMode.wrappedValue.dismiss()
+    }
+
+    private func onSelectedApplicationChanged(_ application: LessonPlan.Application?) {
+        if application != nil { shouldShowInfoBannerOnAppear = true }
+    }
+
+    private func onAppear() {
+        if shouldShowInfoBannerOnAppear { isShowingInfoBanner = true }
     }
 
     func handleRoute(_ route: Route) {
@@ -84,11 +98,41 @@ struct LessonPlanApplicationsView: View, RoutableView {
     }
 }
 
+struct LessonPlanApplicationCell: View {
+    private enum Const {
+        static let avatarSize = .spacingUnit * 14
+    }
+
+    var application: LessonPlan.Application
+
+    init(_ application: LessonPlan.Application) {
+        self.application = application
+    }
+
+    var body: some View {
+        VStack(spacing: .spacingExtraSmall) {
+            LessonPlanTutorAvatarView(application.tutor, thumbnail: true)
+                .frame(width: Const.avatarSize, height: Const.avatarSize)
+            Text(application.tutor.name)
+                .rythmicoFont(.bodyBold)
+                .foregroundColor(.rythmicoForeground)
+                .lineLimit(1)
+                .minimumScaleFactor(0.6)
+        }
+        .padding(.vertical, .spacingExtraLarge)
+        .padding(.horizontal, .spacingSmall)
+        .frame(maxWidth: .infinity)
+        .modifier(RoundedShadowContainer())
+    }
+}
+
 #if DEBUG
 struct LessonPlanApplicationsView_Previews: PreviewProvider {
     static var previews: some View {
-        LessonPlanApplicationsView(.reviewingJackGuitarPlanStub)
-//            .environment(\.sizeCategory, ContentSizeCategory.accessibilityExtraLarge)
+        NavigationView {
+            LessonPlanApplicationsView(.reviewingJackGuitarPlanStub)
+        }
+//        .environment(\.sizeCategory, .accessibilityExtraLarge)
     }
 }
 #endif
