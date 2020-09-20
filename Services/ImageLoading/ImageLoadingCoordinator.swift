9b3833b265755b0769ced627dfccbe77434cf74d
davdroman <d@vidroman.dev>
2020-09-21 00:37:33 +0100

    Refactor image loading service

--- Services/ImageLoading/ImageLoadingCoordinator.swift
+++ Services/ImageLoading/ImageLoadingCoordinator.swift
@@ -15,7 +15,7 @@ final class ImageLoadingCoordinator: FailableActivityCoordinator<UIImage> {
         switch imageReference {
         case .url(let url):
             cancellable = service.load(url) { result in
-                self.state = .finished(result.map { $0.withRenderingMode(.alwaysOriginal) })
+                self.state = .finished(result)
             }
         case .assetName(let assetName):
             self.state = .finished(.success(UIImage(named: assetName)!))
