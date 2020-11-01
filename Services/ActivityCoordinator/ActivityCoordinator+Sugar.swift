620241270258f3dc99163e1efa884468a04a21d8
davdroman <d@vidroman.dev>
2020-11-01 18:05:20 +0000

    Split enum property sugar into domain files

---
+++ Services/ActivityCoordinator/ActivityCoordinator+Sugar.swift
@@ -0,0 +1,51 @@
+import Foundation
+import Sugar
+
+extension ActivityCoordinator.State {
+    var isReady: Bool {
+        guard case .ready = self else { return false }
+        return true
+    }
+
+    var isLoading: Bool {
+        guard case .loading = self else { return false }
+        return true
+    }
+
+    var isSuspended: Bool {
+        guard case .suspended = self else { return false }
+        return true
+    }
+
+    var isFinished: Bool {
+        finishedValue != nil
+    }
+
+    var finishedValue: Output? {
+        guard case .finished(let output) = self else { return nil }
+        return output
+    }
+
+    var isIdle: Bool {
+        guard case .idle = self else { return false }
+        return true
+    }
+}
+
+extension ActivityCoordinator.State where Output: AnyResult {
+    var successValue: Output.Success? {
+        finishedValue?.successValue
+    }
+
+    var failureValue: Output.Failure? {
+        finishedValue?.failureValue
+    }
+
+    var isSuccess: Bool {
+        successValue != nil
+    }
+
+    var isFailure: Bool {
+        failureValue != nil
+    }
+}
---
+++ Services/PushNotifications/PushNotificationAuthorizationCoordinator+Sugar.swift
@@ -0,0 +1,42 @@
+import Foundation
+
+extension PushNotificationAuthorizationCoordinator.Status {
+    var isNotDetermined: Bool {
+        guard case .notDetermined = self else { return false }
+        return true
+    }
+
+    var isAuthorizing: Bool {
+        guard case .authorizing = self else { return false }
+        return true
+    }
+
+    var isFailed: Bool {
+        failedValue != nil
+    }
+
+    var failedValue: Error? {
+        guard case .failed(let error) = self else { return nil }
+        return error
+    }
+
+    var isDenied: Bool {
+        guard case .denied = self else { return false }
+        return true
+    }
+
+    var isAuthorized: Bool {
+        guard case .authorized = self else { return false }
+        return true
+    }
+
+    var isDetermined: Bool {
+        switch self {
+        case .notDetermined, .authorizing, .failed:
+            return false
+        case .denied, .authorized:
+            return true
+        }
+    }
+}
+
