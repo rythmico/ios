05edcc51439f03b61a22ec3acc86b9194909c33b
davdroman <d@vidroman.dev>
2020-11-01 18:00:41 +0000

    Improve `ImageAsset` sugar

--- Sugar/AssetImage+Sugar.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-extension ImageAsset: Equatable {
-    static func == (lhs: Self, rhs: Self) -> Bool {
-        lhs.name == rhs.name
-    }
-}
---
+++ Sugar/ImageAsset+Sugar.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension ImageAsset: Hashable, Equatable {
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(name)
+    }
+
+    static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.hashValue == rhs.hashValue
+    }
+}
