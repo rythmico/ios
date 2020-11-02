882373faf0ba05d7799e34f3de6b0dbb4e544584
davdroman <d@vidroman.dev>
2020-11-02 23:31:34 +0000

    Implement `URLOpener.canOpenURL`

--- Services/URLOpener/URLOpenerDoubles.swift
+++ Services/URLOpener/URLOpenerDoubles.swift
@@ -1,13 +1,26 @@
 import Foundation
 
 final class URLOpenerSpy: URLOpener {
-    var openCount = 0
+    private(set) var openCount = 0
+    private(set) var canOpenCount = 0
+
+    var canOpenURLs: Bool
+
+    init(canOpenURLs: Bool = true) {
+        self.canOpenURLs = canOpenURLs
+    }
 
     func open(_ url: URL) {
         openCount += 1
+    }
+
+    func canOpenURL(_ url: URL) -> Bool {
+        canOpenCount += 1
+        return canOpenURLs
     }
 }
 
 final class URLOpenerDummy: URLOpener {
     func open(_ url: URL) {}
+    func canOpenURL(_ url: URL) -> Bool { true }
 }
