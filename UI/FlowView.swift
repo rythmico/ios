544bc1a169a6f43700374a5cd3e7aa06b5612898
davdroman <d@vidroman.dev>
2021-01-26 14:34:44 +0000

    Gardening

--- UI/FlowView.swift
+++ UI/FlowView.swift
@@ -1,40 +1,5 @@
 import SwiftUI
 
-protocol Numbered: Comparable {
-    var index: Int { get }
-}
-
-extension Numbered {
-    static func < (lhs: Self, rhs: Self) -> Bool {
-        lhs.index < rhs.index
-    }
-}
-
-extension Numbered where Self: CaseIterable, AllCases.Element: Hashable, AllCases: RandomAccessCollection, AllCases.Index == Int {
-    var index: Int {
-        guard let n = Self.allCases.firstIndex(of: self) else {
-            preconditionFailure("Case '\(type(of: self)).\(self)' not contained within 'CaseIterable.allCases'.")
-        }
-        return n
-    }
-}
-
-protocol Countable {
-    static var count: Int { get }
-}
-
-extension Countable where Self: CaseIterable {
-    static var count: Int {
-        allCases.count
-    }
-}
-
-protocol Flow: ObservableObject {
-    associatedtype Step: Numbered, Countable
-    var previousStep: Step? { get }
-    var currentStep: Step { get }
-}
-
 struct FlowView<FlowType: Flow, Content: View>: View {
     typealias Step = FlowType.Step
 
