401f2b823751c3b1a10cab4e42dd11e37614ae27
davdroman <d@vidroman.dev>
2021-02-15 19:06:38 +0000

    Add header image and titles to tutor verification states

--- RythmicoTutor/Root/Verification/TutorStatusBanner.swift
+++ RythmicoTutor/Root/Verification/TutorStatusBanner.swift
@@ -8,19 +8,27 @@ struct TutorStatusBanner: View {
     var status: TutorStatus
 
     var body: some View {
-        VStack(spacing: .spacingUnit * 12) {
-            Image(decorative: Asset.appLogo.name)
-                .resizable()
-                .scaledToFit()
-                .frame(width: 68)
-            Text(status.description)
-                .foregroundColor(foregroundColor)
-                .multilineTextAlignment(.center)
-                .lineSpacing(.spacingUnit)
-                .frame(maxWidth: .spacingMax)
-                .padding(.horizontal, .spacingUnit * 10)
-                .transition(.opacity)
-                .id(status.description.hashValue)
+        VStack(spacing: .spacingExtraLarge) {
+            if let image = status.image {
+                Image(decorative: image.name)
+                    .resizable()
+                    .scaledToFit()
+                    .padding(.horizontal, .spacingUnit * 8)
+            }
+            VStack(spacing: .spacingMedium) {
+                Text(status.title)
+                    .font(.system(size: 19, weight: .bold))
+                    .transition(.opacity)
+                    .id(status.title.hashValue)
+                Text(status.description)
+                    .transition(.opacity)
+                    .id(status.description.hashValue)
+            }
+            .foregroundColor(foregroundColor)
+            .multilineTextAlignment(.center)
+            .lineSpacing(.spacingUnit)
+            .frame(maxWidth: .spacingMax)
+            .padding(.horizontal, .spacingUnit * 10)
             enableNotificationsAction.map { Button("Notify Me", action: $0) }
         }
         .animation(.rythmicoSpring(duration: .durationMedium), value: enableNotificationsAction != nil)
@@ -47,20 +55,34 @@ struct TutorStatusBanner: View {
 }
 
 private extension TutorStatus {
+    var image: ImageAsset? {
+        switch self {
+        case .notCurated:
+            return Asset.graphicsVerificationWaiting
+        case .notDBSChecked:
+            return Asset.graphicsVerificationWaiting
+        case .notRegistered, .verified:
+            return nil
+        }
+    }
+
+    var title: String {
+        switch self {
+        case .notCurated:
+            return "We’re reviewing your submission"
+        case .notDBSChecked:
+            return "We’re awaiting your DBS Check result"
+        case .notRegistered, .verified:
+            return .empty
+        }
+    }
+
     var description: String {
         switch self {
         case .notCurated:
-            return  """
-                    Thank you for signing up as a Rythmico Tutor.
-
-                    We will review your submission and reach out to you within a few days.
-                    """
+            return "Thank you for signing up as a Rythmico Tutor. We will review your profile and reach out to you within a few days."
         case .notDBSChecked:
-            return  """
-                    Your mandatory DBS check form is now ready.
-
-                    Please follow the link sent to your inbox provided by uCheck.
-                    """
+            return "Your mandatory DBS check form is pending your submission. Please follow the link sent to your inbox provided by uCheck."
         case .notRegistered, .verified:
             return .empty
         }
@@ -70,7 +92,11 @@ private extension TutorStatus {
 #if DEBUG
 struct TutorStatusBanner_Previews: PreviewProvider {
     static var previews: some View {
-        TutorStatusBanner(status: .notCurated)
+        Group {
+            TutorStatusBanner(status: .notCurated)
+            TutorStatusBanner(status: .notDBSChecked)
+        }
+        .previewDevices()
     }
 }
 #endif
