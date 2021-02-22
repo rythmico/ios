51a65135994754f04dfa24ba7a822378ad6ffe0c
davdroman <d@vidroman.dev>
2021-02-22 18:22:23 +0000

    Remove Sugar framework

--- Sugar/Bool+Sugar.swift
+++
@@ -1,5 +0,0 @@
-import Foundation
-
-extension Bool {
-    public var not: Bool { !self }
-}
--- Sugar/Typealiases.swift
+++
@@ -1,9 +0,0 @@
-import Swift
-
-public typealias Action = () -> Void
-public typealias Handler<T> = (T) -> Void
-
-public typealias ResultHandler<V, E: Error> = Handler<Result<V, E>>
-
-public typealias SimpleResult<V> = Result<V, Error>
-public typealias SimpleResultHandler<V> = Handler<SimpleResult<V>>
