21ee8abec36635c7027ed8e6ad11bb7e330cd248
davdroman <d@vidroman.dev>
2021-03-16 03:31:52 +0000

    Implement MailOpener

---
+++ Services/AppOpeners/MailOpener/MailOpener.swift
@@ -0,0 +1,55 @@
+import Foundation
+import FoundationSugar
+
+extension URLOpener {
+    func open(_ link: MailLink) throws {
+        open(try link.url())
+    }
+}
+
+enum MailLink {
+    case mail(
+        to: [String]? = nil,
+        cc: [String]? = nil,
+        subject: String? = nil,
+        body: String? = nil
+    )
+}
+
+fileprivate extension MailLink {
+    var scheme: String {
+        switch self {
+        case .mail:
+            return "mailto"
+        }
+    }
+
+    var host: String {
+        switch self {
+        case .mail(let to, _, _, _):
+            return to?.joined(separator: ",") ?? .empty
+        }
+    }
+
+    @ArrayBuilder<URLQueryItem>
+    var queryItems: [URLQueryItem] {
+        switch self {
+        case let .mail(_, cc, subject, body):
+            if let cc = cc {
+                URLQueryItem(name: "cc", value: cc.joined(separator: ","))
+            }
+            if let subject = subject {
+                URLQueryItem(name: "subject", value: subject)
+            }
+            if let body = body {
+                URLQueryItem(name: "body", value: body)
+            }
+        }
+    }
+
+    func url() throws -> URL {
+        try URL(
+            string: URL(scheme: scheme, host: host, queryItems: queryItems).absoluteString.replacingOccurrences(of: "mailto://", with: "mailto:")
+        )!
+    }
+}
