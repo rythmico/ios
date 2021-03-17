2069b5f0c918177a6e7dcbde9063bf1986aaf38c
davdroman <d@vidroman.dev>
2021-03-17 18:43:55 +0000

    Add `Result.init(value:error)` sugar

--- FoundationSugar/Sources/FoundationSugar/Result+Sugar.swift
+++ FoundationSugar/Sources/FoundationSugar/Result+Sugar.swift
@@ -1,5 +1,18 @@
 import Foundation
 
+extension Result {
+    public init?(value: Success?, error: Failure?) {
+        switch (value, error) {
+        case (let value?, _):
+            self = .success(value)
+        case (_, let error?):
+            self = .failure(error)
+        case (nil, nil):
+            return nil
+        }
+    }
+}
+
 extension Result where Success == Void {
     public static var success: Result { .success(()) }
 }
