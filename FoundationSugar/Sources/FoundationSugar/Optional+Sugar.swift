af23161874f9c1dcc264121b0eec92dd71e068cf
davdroman <d@vidroman.dev>
2021-03-19 22:53:09 +0000

    Implement `!!` operator

--- FoundationSugar/Sources/FoundationSugar/Optional+Sugar.swift
+++ FoundationSugar/Sources/FoundationSugar/Optional+Sugar.swift
@@ -23,3 +23,18 @@ extension Optional where Wrapped: NSNumber {
         self.map { $0.intValue == 0 } ?? true
     }
 }
+
+precedencegroup NilAssertingPrecedence {
+    associativity: left
+    higherThan: NilCoalescingPrecedence
+}
+
+infix operator !! : NilAssertingPrecedence
+
+public func !! <T>(optional: T?, exitClosure: @autoclosure () -> Never) -> T {
+    guard let value = optional else {
+        exitClosure()
+    }
+    return value
+}
+
