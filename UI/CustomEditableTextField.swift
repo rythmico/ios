f5668efae77a65c421181cd61770b4d0f0d18515
davdroman <d@vidroman.dev>
2021-03-20 16:57:11 +0000

    Implement `CustomEditableTextField`

---
+++ UI/CustomEditableTextField.swift
@@ -0,0 +1,35 @@
+import SwiftUI
+import FoundationSugar
+
+struct CustomEditableTextField<EditingView: View>: View {
+    var placeholder: String
+    var text: String?
+    var isEditing: Bool = false
+    var editAction: Action
+    @ViewBuilder
+    var editingView: EditingView
+
+    @Namespace
+    private var animation
+
+    var body: some View {
+        ZStack {
+            switch isEditing {
+            case false:
+                textField.matchedGeometryEffect(id: animation, in: animation, properties: .size)
+            case true:
+                editingView.matchedGeometryEffect(id: animation, in: animation, properties: .size)
+            }
+        }
+        .modifier(RoundedThinOutlineContainer(padded: false))
+    }
+
+    private var textField: some View {
+        NonEditableTextField(
+            placeholder: placeholder,
+            text: text,
+            outlined: false,
+            tapAction: editAction
+        )
+    }
+}
