ba9fd588bbd45b0710b93a1f6d773858fca40b5c
davdroman <d@vidroman.dev>
2021-04-08 00:26:30 +0100

    Implement `AnalyticsPropsBuilder`

---
+++ Services/Analytics/AnalyticsPropsBuilder.swift
@@ -0,0 +1,6 @@
+import Foundation
+import FoundationSugar
+import typealias Mixpanel.Properties
+
+typealias AnalyticsProps = Mixpanel.Properties
+typealias AnalyticsPropsBuilder = DictionaryBuilder<Mixpanel.Properties.Key, Mixpanel.Properties.Value>
