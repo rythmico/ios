d723e044344abfdb1a86a44ab01ee37acf00759e
davdroman <d@vidroman.dev>
2021-04-08 16:56:28 +0100

    Implement `AnalyticsService.track(event:)`

--- Services/Analytics/AnalyticsService.swift
+++ Services/Analytics/AnalyticsService.swift
@@ -5,7 +5,7 @@ import class Mixpanel.MixpanelInstance
 protocol AnalyticsServiceProtocol {
     func identify(_ profile: AnalyticsUserProfile)
 //    func time(event: String)
-//    func track(event: String?, properties: Properties?)
+    func track(event: AnalyticsEvent)
     func reset()
 }
 
@@ -13,5 +13,9 @@ extension MixpanelInstance: AnalyticsServiceProtocol {
     func identify(_ profile: AnalyticsUserProfile) {
         identify(distinctId: profile.id, usePeople: true)
         people.set(properties: profile.rawAnalyticsValue)
+    }
+
+    func track(event: AnalyticsEvent) {
+        track(event: event.name.rawValue, properties: event.props)
     }
 }
--- Services/Analytics/AnalyticsServiceDoubles.swift
+++ Services/Analytics/AnalyticsServiceDoubles.swift
@@ -2,5 +2,6 @@ import Foundation
 
 final class AnalyticsServiceDummy: AnalyticsServiceProtocol {
     func identify(_ profile: AnalyticsUserProfile) {}
+    func track(event: AnalyticsEvent) {}
     func reset() {}
 }
