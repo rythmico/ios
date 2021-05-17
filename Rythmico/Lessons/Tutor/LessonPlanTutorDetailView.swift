f39f29ee81dbc403f5079515b2d0d6b730b67c34
davdroman <d@vidroman.dev>
2021-05-17 17:51:50 +0100

    Implement `LessonPlanTutorDetailView`

---
+++ Rythmico/Lessons/Tutor/LessonPlanTutorDetailView.swift
@@ -0,0 +1,50 @@
+import SwiftUI
+import ComposableNavigator
+
+struct LessonPlanTutorDetailScreen: Screen {
+    var lessonPlan: LessonPlan
+    var tutor: Tutor
+    let presentationStyle: ScreenPresentationStyle = .push
+
+    struct Builder: NavigationTree {
+        var builder: some PathBuilder {
+            Screen(
+                content: { (screen: LessonPlanTutorDetailScreen) in
+                    LessonPlanTutorDetailView(lessonPlan: screen.lessonPlan, tutor: screen.tutor)
+                },
+                nesting: {
+                    VideoCarouselPlayerScreen.Builder()
+                    PhotoCarouselDetailScreen.Builder()
+                }
+            )
+        }
+    }
+}
+
+struct LessonPlanTutorDetailView: View {
+    typealias HeaderView = LessonPlanApplicationDetailHeaderView
+    typealias PortfolioView = LessonPlanApplicationDetailAboutView
+
+    @StateObject
+    private var coordinator = Current.portfolioFetchingCoordinator()
+
+    // TODO: consume 'tutor.instruments' to remove this property.
+    var lessonPlan: LessonPlan
+    var tutor: Tutor
+
+    var body: some View {
+        VStack(spacing: .spacingExtraLarge) {
+            HeaderView(lessonPlan: lessonPlan, tutor: tutor)
+            PortfolioView(coordinator: coordinator, tutor: tutor, topPadding: 0)
+        }
+        .backgroundColor(.rythmicoBackground)
+    }
+}
+
+#if DEBUG
+struct LessonPlanTutorDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        LessonPlanTutorDetailView(lessonPlan: .charlottePianoPlanStub, tutor: .charlotteStub)
+    }
+}
+#endif
