8784d0a3e25754f928439b3db58d179cb0c70f87
davdroman <d@vidroman.dev>
2021-05-19 16:17:55 +0100

    Implement `Sequence.sorted(_:by:)` convenience function

--- FoundationSugar/Sources/FoundationSugar/Sequence+Sugar.swift
+++ FoundationSugar/Sources/FoundationSugar/Sequence+Sugar.swift
@@ -24,3 +24,16 @@ extension Sequence where Element: Sequence {
         flatMap { $0 }
     }
 }
+
+extension Sequence {
+    public func sorted<T>(
+        _ value: KeyPath<Element, T>,
+        by areInIncreasingOrder: (T, T) throws -> Bool
+    ) rethrows -> [Element] {
+        try sorted(by: { try areInIncreasingOrder($0[keyPath: value], $1[keyPath: value]) })
+    }
+
+    public func sorted<T>(_ value: KeyPath<Element, T>) -> [Element] where T: Comparable {
+        sorted(value, by: <)
+    }
+}
