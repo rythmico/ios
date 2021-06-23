33a70a88d0ecf203c777852fbf8f185917d09159
davdroman <d@vidroman.dev>
2021-06-23 01:49:36 +0100

    Gardening

--- Core/DateComponents+Sugar.swift
+++ Core/DateComponents+Sugar.swift
@@ -11,6 +11,6 @@ extension DateComponents {
             allowedUnits: allowedUnits,
             style: style,
             includesTimeRemainingPhrase: includesTimeRemainingPhrase
-        ).string(for: self) !! preconditionFailure("nil for input '\(self)'")
+        ).string(from: self) !! preconditionFailure("nil for input '\(self)'")
     }
 }
