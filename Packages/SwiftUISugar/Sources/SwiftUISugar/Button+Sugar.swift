b282c5c902d748d1fc1d45dd7e2b64e81c210a12
davdroman <d@vidroman.dev>
2021-06-25 00:56:26 +0100

    Move `Button+Sugar.swift` into `SwiftUISugar`

--- Packages/SwiftUISugar/Sources/SwiftUISugar/Button+Sugar.swift
+++ Packages/SwiftUISugar/Sources/SwiftUISugar/Button+Sugar.swift
@@ -1,7 +1,5 @@
-import SwiftUI
-
 extension Button where Label == Text {
-    init(_ titleKey: LocalizedStringKey, action: (() -> Void)?) {
+    public init(_ titleKey: LocalizedStringKey, action: (() -> Void)?) {
         self.init(titleKey, action: action ?? {})
     }
 }
