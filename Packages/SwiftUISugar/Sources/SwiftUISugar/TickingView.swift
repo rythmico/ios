e631b916d87398271f84b24861ea5c9bf9522ba4
davdroman <d@vidroman.dev>
2021-06-25 02:05:28 +0100

    Move `TickingView.swift` into `SwiftUISugar`

--- Packages/SwiftUISugar/Sources/SwiftUISugar/TickingView.swift
+++ Packages/SwiftUISugar/Sources/SwiftUISugar/TickingView.swift
@@ -1,19 +1,19 @@
-import SwiftUI
+// TODO: replace with SwiftUI.TimelineView in iOS 15
 
 /// Time-aware view.
-struct TickingView<Content: View>: View {
+public struct TickingView<Content: View>: View {
     private let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     private let contentBuilder: () -> Content
 
     @State
     private var content: Content
 
-    init(@ViewBuilder content: @escaping () -> Content) {
+    public init(@ViewBuilder content: @escaping () -> Content) {
         self.contentBuilder = content
         self._content = State(initialValue: content())
     }
 
-    var body: some View {
+    public var body: some View {
         ZStack {
             content
         }
@@ -24,10 +24,11 @@ struct TickingView<Content: View>: View {
     }
 }
 
-struct TickingText: View {
-    var body: TickingView<Text>
+// TODO: allow TextStyle or remove altogether
+public struct TickingText: View {
+    public var body: TickingView<Text>
 
-    init<S: StringProtocol>(_ string: @autoclosure @escaping () -> S) {
+    public init<S: StringProtocol>(_ string: @autoclosure @escaping () -> S) {
         body = TickingView { Text(string()) }
     }
 }
