1a184b82c1f801f81da2a62b58e2bbf534c477b7
davdroman <d@vidroman.dev>
2021-06-25 02:02:10 +0100

    Move `Text+Sugar.swift` into `SwiftUISugar`

--- Packages/SwiftUISugar/Sources/SwiftUISugar/Text+Sugar.swift
+++ Packages/SwiftUISugar/Sources/SwiftUISugar/Text+Sugar.swift
@@ -1,10 +1,8 @@
-import SwiftUI
-
 // TODO: remove in Swift 5.5, extend `StringProtocol` instead (`String+Sugar.swift:5`)
 // https://github.com/apple/swift-evolution/blob/main/proposals/0299-extend-generic-static-member-lookup.md
 
 @available(swift, obsoleted: 5.5)
 extension Text {
-    static var whitespace: Text { Text(String.whitespace) }
-    static var newline: Text { Text(String.newline) }
+    public static var whitespace: Text { Text(String.whitespace) }
+    public static var newline: Text { Text(String.newline) }
 }
