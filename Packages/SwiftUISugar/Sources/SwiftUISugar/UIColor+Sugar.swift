ef6be4c5c7cf2234c9d7c8af10a97a1b8d345597
davdroman <d@vidroman.dev>
2021-06-26 02:22:25 +0100

    Move `UIColor+Sugar.swift` to `SwiftUISugar`

--- Packages/SwiftUISugar/Sources/SwiftUISugar/UIColor+Sugar.swift
+++ Packages/SwiftUISugar/Sources/SwiftUISugar/UIColor+Sugar.swift
@@ -1,43 +1,41 @@
-import SwiftUI
-
 extension Color {
-    init(light: UIColor, dark: UIColor) {
+    public init(light: UIColor, dark: UIColor) {
         self.init(UIColor(light: light, dark: dark))
     }
 
-    init(light: UIColor, dark: Int) {
+    public init(light: UIColor, dark: Int) {
         self.init(UIColor(light: light, dark: dark))
     }
 
-    init(light: Int, dark: UIColor) {
+    public init(light: Int, dark: UIColor) {
         self.init(UIColor(light: light, dark: dark))
     }
 
-    init(light: Int, dark: Int) {
+    public init(light: Int, dark: Int) {
         self.init(UIColor(light: light, dark: dark))
     }
 }
 
 extension UIColor {
-    convenience init(light: UIColor, dark: UIColor) {
+    public convenience init(light: UIColor, dark: UIColor) {
         self.init { $0.userInterfaceStyle == .light ? light : dark }
     }
 
-    convenience init(light: UIColor, dark: Int) {
+    public convenience init(light: UIColor, dark: Int) {
         self.init(light: light, dark: .init(hex: dark))
     }
 
-    convenience init(light: Int, dark: UIColor) {
+    public convenience init(light: Int, dark: UIColor) {
         self.init(light: .init(hex: light), dark: dark)
     }
 
-    convenience init(light: Int, dark: Int) {
+    public convenience init(light: Int, dark: Int) {
         self.init(light: .init(hex: light), dark: .init(hex: dark))
     }
 }
 
 extension UIColor {
-    convenience init(hex: Int, alpha: CGFloat = 1) {
+    public convenience init(hex: Int, alpha: CGFloat = 1) {
         let divisor = CGFloat(255)
         let red     = CGFloat((hex & 0xFF0000) >> 16) / divisor
         let green   = CGFloat((hex & 0x00FF00) >>  8) / divisor
