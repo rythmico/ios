75138da8d5f09cc872ad223a1ab47d47e2694100
davdroman <d@vidroman.dev>
2021-07-20 15:20:41 +0100

    Implement `LessonPlansView`

---
+++ Rythmico/Lessons/LessonPlansView.swift
@@ -0,0 +1,58 @@
+import SwiftUISugar
+import ComposableNavigator
+
+struct LessonPlansScreen: Screen {
+    let presentationStyle: ScreenPresentationStyle = .push
+
+    struct Builder: NavigationTree {
+        var builder: some PathBuilder {
+            Screen(
+                content: { (screen: LessonPlansScreen) in
+                    LessonPlansView()
+                },
+                nesting: {
+                    LessonPlanDetailScreen.Builder()
+                    LessonPlanApplicationsScreen.Builder()
+                }
+            )
+        }
+    }
+}
+
+struct LessonPlansView: View {
+    @ObservedObject
+    private var coordinator = Current.lessonPlanFetchingCoordinator
+    @ObservedObject
+    private var repository = Current.lessonPlanRepository
+
+    var body: some View {
+        TitleContentView(title: title) {
+            LessonPlansCollectionView(lessonPlans: repository.items)
+        }
+        .backgroundColor(.rythmicoBackground)
+        .accentColor(.rythmicoPurple)
+        .navigationBarTitle(title)
+        .navigationBarTitleDisplayMode(.inline)
+        .navigationBarItems(leading: leadingItem)
+        .onAppear(perform: coordinator.startToIdle)
+        .onSuccess(coordinator, perform: repository.setItems)
+        .alertOnFailure(coordinator)
+    }
+
+    private var title: String { "Lesson Plans" }
+
+    @ViewBuilder
+    private var leadingItem: some View {
+        if coordinator.state.isLoading {
+            ActivityIndicator(color: .rythmicoGray90)
+        }
+    }
+}
+
+#if DEBUG
+struct LessonPlansView_Previews: PreviewProvider {
+    static var previews: some View {
+        LessonPlansView()
+    }
+}
+#endif
