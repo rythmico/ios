d5ce14b0fd3b097bbe7a3a90c0aa36e4ccc0b193
davdroman <d@vidroman.dev>
2021-07-28 17:42:23 +0100

    Tidy up `UIColor.hex` initializer

--- Packages/SwiftUISugar/Sources/SwiftUISugar/UIColor+Sugar.swift
+++ Packages/SwiftUISugar/Sources/SwiftUISugar/UIColor+Sugar.swift
@@ -36,10 +36,11 @@ extension UIColor {
 
 extension UIColor {
     public convenience init(hex: UInt, alpha: CGFloat = 1) {
-        let divisor = CGFloat(255)
-        let red     = CGFloat((hex & 0xFF0000) >> 16) / divisor
-        let green   = CGFloat((hex & 0x00FF00) >>  8) / divisor
-        let blue    = CGFloat( hex & 0x0000FF       ) / divisor
-        self.init(red: red, green: green, blue: blue, alpha: alpha)
+        self.init(
+            red: CGFloat((hex & 0xFF0000) >> 16) / 255,
+            green: CGFloat((hex & 0x00FF00) >> 8) / 255,
+            blue: CGFloat(hex & 0x0000FF) / 255,
+            alpha: alpha
+        )
     }
 }
