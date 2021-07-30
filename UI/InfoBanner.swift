796dd25e065cab7e3f47a3d0337ba77521804fb2
davdroman <d@vidroman.dev>
2021-07-30 17:03:05 +0100

    Use `Container` in `InfoBanner`

--- UI/InfoBanner.swift
+++ UI/InfoBanner.swift
@@ -1,23 +1,29 @@
-import SwiftUI
+import SwiftUISugar
 
 struct InfoBanner: View {
-    var text: String
+    let text: String
 
     var body: some View {
-        HStack(alignment: .firstTextBaseline, spacing: .grid(3)) {
-            Image(decorative: Asset.Icon.Label.info.name)
-                .renderingMode(.template)
-                .alignmentGuide(.firstTextBaseline) { $0[.bottom] - .grid(1) }
-            Text(text)
-                .rythmicoTextStyle(.callout)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .fixedSize(horizontal: false, vertical: true)
+        Container(
+            style: .init(
+                fill: .rythmico.azureBlue,
+                shape: .roundedRectangle(radius: 4, style: .continuous),
+                border: .none
+            )
+        ) {
+            HStack(alignment: .firstTextBaseline, spacing: .grid(3)) {
+                Image(decorative: Asset.Icon.Label.info.name)
+                    .renderingMode(.template)
+                    .alignmentGuide(.firstTextBaseline) { $0[.bottom] - .grid(1) }
+                Text(text)
+                    .rythmicoTextStyle(.callout)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .fixedSize(horizontal: false, vertical: true)
+            }
+            .padding(.grid(4))
+            .foregroundColor(.rythmico.foreground)
+            .accentColor(.rythmico.foreground)
         }
-        .padding(.grid(4))
-        .foregroundColor(.rythmico.foreground)
-        .accentColor(.rythmico.foreground)
-        .background(Color.rythmico.azureBlue)
-        .clipShape(RoundedRectangle(cornerRadius: .grid(1), style: .continuous))
     }
 }
 
