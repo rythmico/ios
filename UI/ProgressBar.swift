e37df3a44eb61fd70fb663014cec940bca4a56bc
davdroman <d@vidroman.dev>
2021-07-30 01:20:58 +0100

    Update `ProgressBar` style

--- UI/ProgressBar.swift
+++ UI/ProgressBar.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct ProgressBar: View {
     private enum Const {
-        static let height: CGFloat = 8
+        static let height: CGFloat = .grid(4)
     }
 
     private let progress: Double
@@ -14,12 +14,47 @@ struct ProgressBar: View {
     var body: some View {
         GeometryReader { metrics in
             ZStack(alignment: .leading) {
-                Color.rythmico.gray10.cornerRadius(.greatestFiniteMagnitude, antialiased: true)
-                Color.rythmico.purple
-                    .cornerRadius(.greatestFiniteMagnitude, antialiased: true)
-                    .frame(width: metrics.size.width * CGFloat(progress))
+                Capsule(style: .circular).fill(Color.rythmico.picoteeBlue)
+                Capsule(style: .circular).fill(Color.rythmico.azureBlue)
+                    .frame(maxWidth: barWidth(metrics), maxHeight: barHeight(metrics))
+                    .padding(.horizontal, barHorizontalPadding(metrics))
             }
         }
         .frame(height: Const.height)
     }
+
+    private func barWidth(_ metrics: GeometryProxy) -> CGFloat {
+        metrics.size.width * CGFloat(progress)
+    }
+
+    private func barHeight(_ metrics: GeometryProxy) -> CGFloat {
+        metrics.size.height / 2
+    }
+
+    private func barHorizontalPadding(_ metrics: GeometryProxy) -> CGFloat {
+        (Const.height - barHeight(metrics)) / 2
+    }
 }
+
+#if DEBUG
+struct ProgressBar_PreviewContainer: View {
+    @State var progress: Double = 0
+
+    var body: some View {
+        ProgressBar(progress: progress)
+            .onAppear {
+                withAnimation(.default.speed(0.01)) {
+                    progress = 1
+                }
+            }
+    }
+}
+
+struct ProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressBar_PreviewContainer()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
+#endif
