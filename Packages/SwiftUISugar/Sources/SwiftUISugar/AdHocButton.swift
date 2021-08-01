a7471be3ea1ed6a7649c8f87efb8d1df7604eca3
davdroman <d@vidroman.dev>
2021-08-01 02:25:08 +0100

    Introduce `AdHocButton`

---
+++ Packages/SwiftUISugar/Sources/SwiftUISugar/AdHocButton.swift
@@ -0,0 +1,33 @@
+public struct AdHocButton<Content: View>: View {
+    public typealias MakeBody = AdHocButtonStyleMakeBody<Content>
+
+    private let action: Action
+    private let makeBody: MakeBody
+
+    public init(action: @escaping Action, @ViewBuilder makeBody: @escaping MakeBody) {
+        self.action = action
+        self.makeBody = makeBody
+    }
+
+    public var body: some View {
+        Button(action: action, label: EmptyView.init).buttonStyle(AdHocButtonStyle(makeBody: makeBody))
+    }
+}
+
+public struct AdHocButtonStyle<Body: View>: ButtonStyle {
+    @Environment(\.isEnabled) private var isEnabled
+
+    public typealias MakeBody = AdHocButtonStyleMakeBody<Body>
+
+    private let makeBody: MakeBody
+
+    public init(@ViewBuilder makeBody: @escaping MakeBody) {
+        self.makeBody = makeBody
+    }
+
+    public func makeBody(configuration: Configuration) -> Body {
+        makeBody(configuration, isEnabled)
+    }
+}
+
+public typealias AdHocButtonStyleMakeBody<Body> = (_ configuration: ButtonStyleConfiguration, _ isEnabled: Bool) -> Body
