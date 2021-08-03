2cbe844ed22f7eb79b7049a3f970ecdb5834b2e4
davdroman <d@vidroman.dev>
2021-08-03 17:08:45 +0100

    Introduce `HorizontalInsets` and `VerticalInsets` + view padding sugar

--- Packages/SwiftUISugar/Sources/SwiftUISugar/EdgeInsets+Sugar.swift
+++ Packages/SwiftUISugar/Sources/SwiftUISugar/EdgeInsets+Sugar.swift
@@ -1,3 +1,35 @@
+public struct HorizontalInsets: Equatable {
+    public var leading: CGFloat
+    public var trailing: CGFloat
+
+    public init(leading: CGFloat = 0, trailing: CGFloat = 0) {
+        self.leading = leading
+        self.trailing = trailing
+    }
+
+    public init(_ horizontal: CGFloat) {
+        self.init(leading: horizontal, trailing: horizontal)
+    }
+
+    public static var zero: HorizontalInsets { .init() }
+}
+
+public struct VerticalInsets: Equatable {
+    public var top: CGFloat
+    public var bottom: CGFloat
+
+    public init(top: CGFloat = 0, bottom: CGFloat = 0) {
+        self.top = top
+        self.bottom = bottom
+    }
+
+    public init(_ vertical: CGFloat) {
+        self.init(top: vertical, bottom: vertical)
+    }
+
+    public static var zero: VerticalInsets { .init() }
+}
+
 extension EdgeInsets {
     public init(top: CGFloat = 0, leading: CGFloat = 0, bottom: CGFloat = 0, trailing: CGFloat = 0) {
         self.init()
@@ -8,19 +40,30 @@ extension EdgeInsets {
     }
 
     public init(horizontal: CGFloat = 0, vertical: CGFloat = 0) {
-        self.init()
-        self.top = vertical
-        self.leading = horizontal
-        self.bottom = vertical
-        self.trailing = horizontal
+        self.init(
+            top: vertical,
+            leading: horizontal,
+            bottom: vertical,
+            trailing: horizontal
+        )
+    }
+
+    public init(horizontal: HorizontalInsets = .zero, vertical: VerticalInsets = .zero) {
+        self.init(
+            top: vertical.top,
+            leading: horizontal.leading,
+            bottom: vertical.bottom,
+            trailing: horizontal.trailing
+        )
     }
 
     public init(_ all: CGFloat) {
-        self.init()
-        self.top = all
-        self.leading = all
-        self.bottom = all
-        self.trailing = all
+        self.init(
+            top: all,
+            leading: all,
+            bottom: all,
+            trailing: all
+        )
     }
 
     public static var zero: EdgeInsets { .init() }
