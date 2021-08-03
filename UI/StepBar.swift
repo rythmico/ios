e8be7ff06c7a696478b9a3f981b43675005ee538
davdroman <d@vidroman.dev>
2021-08-03 22:45:22 +0100

    Avoid `StepBar` changing size as progress changes

--- UI/StepBar.swift
+++ UI/StepBar.swift
@@ -1,35 +1,51 @@
-import SwiftUI
+import SwiftUISugar
 
 struct StepBar: View {
-    var currentStep: Int
-    var totalSteps: Int
+    let currentStep: Int
+    let totalSteps: Int
 
     init(_ currentStep: Int, of totalSteps: Int) {
-        self.currentStep = currentStep
+        self.currentStep = min(currentStep, totalSteps)
         self.totalSteps = totalSteps
     }
 
     var body: some View {
-        HStack(spacing: 10) {
+        HStack(spacing: 0) {
             ProgressBar(progress: Double(currentStep) / Double(totalSteps))
                 .animation(.rythmicoSpring(duration: .durationMedium))
-            Text("\(currentStep) / \(totalSteps)")
-                .rythmicoTextStyle(.footnoteBold)
-                .foregroundColor(.rythmico.picoteeBlue)
+            ZStack(alignment: .trailing) {
+                progressText(currentStep: currentStep)
+                progressText(currentStep: 50).hidden()
+            }
         }
         .frame(maxWidth: .grid(.max))
         .accessibilityElement()
         .accessibility(label: Text("Step indicator"))
         .accessibility(value: Text("\(currentStep) out of \(totalSteps) steps completed"))
     }
+
+    @ViewBuilder
+    func progressText(currentStep: Int) -> some View {
+        Text("\(currentStep)/\(totalSteps)")
+            .rythmicoTextStyle(.footnoteBold)
+            .foregroundColor(.rythmico.picoteeBlue)
+    }
 }
 
 #if DEBUG
+import Combine
+
 struct StepBar_Previews: PreviewProvider {
+    static var cancellables: [AnyCancellable] = []
+
     static var previews: some View {
-        let steps = 0...4
-        ForEach(steps, id: \.self) { step in
-            StepBar(step, of: steps.upperBound)
+        StatefulPreview(0) { currentStep in
+            StepBar(currentStep.wrappedValue, of: 7)
+                .onAppear {
+                    DispatchQueue.main.schedule(after: .init(.now() + 2), interval: 1) {
+                        currentStep.wrappedValue += 1
+                    }.store(in: &cancellables)
+                }
         }
         .previewLayout(.sizeThatFits)
         .padding()
