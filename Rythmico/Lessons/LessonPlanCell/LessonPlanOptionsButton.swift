7218b8ed0a6eb0ee0f20ef65899f1e12b9b789d9
davdroman <d@vidroman.dev>
2021-08-07 13:21:12 +0100

    Extract lesson plan options button into `LessonPlanOptionsButton`

---
+++ Rythmico/Lessons/LessonPlanCell/LessonPlanOptionsButton.swift
@@ -0,0 +1,61 @@
+import SwiftUISugar
+
+struct LessonPlanOptionsButton: View {
+    @Environment(\.navigator) private var navigator
+    @Environment(\.currentScreen) private var currentScreen
+
+    let lessonPlan: LessonPlan
+    let size: OptionsButton.Size
+    var padding: CGFloat = 0
+
+    var body: some View {
+        if let actions = actions.nilIfEmpty {
+            OptionsButton(size: size, padding: padding, actions)
+                .multiModal {
+                    $0.alert(isPresented: $showingRescheduleAlert) { .reschedulingView(lessonPlan: lessonPlan) }
+                }
+        }
+    }
+
+    @ArrayBuilder<ContextMenuButton>
+    private var actions: [ContextMenuButton] {
+        if let action = showRescheduleAlertAction {
+            .init(title: "Reschedule Plan", icon: Asset.Icon.Action.reschedule, action: action)
+        }
+        if let action = showPauseLessonPlanFormAction {
+            .init(title: "Pause Plan", icon: Asset.Icon.Action.pause, action: action)
+        }
+        if let action = showResumeLessonPlanFormAction {
+            .init(title: "Resume Plan", icon: Asset.Icon.Action.resume, action: action)
+        }
+        if let action = showCancelLessonPlanFormAction {
+            .init(title: "Cancel Plan", icon: Asset.Icon.Action.cancel, action: action)
+        }
+    }
+
+    @State
+    private var showingRescheduleAlert = false
+    private var showRescheduleAlertAction: Action? {
+        !lessonPlan.status.isCancelled && !lessonPlan.status.isPaused // TODO: consume 'LessonPlan.options.reschedule'.
+            ? { showingRescheduleAlert = true }
+            : nil
+    }
+
+    private var showPauseLessonPlanFormAction: Action? {
+        LessonPlanPausingScreen(lessonPlan: lessonPlan).map { screen in
+            { navigator.go(to: screen, on: currentScreen) }
+        }
+    }
+
+    private var showResumeLessonPlanFormAction: Action? {
+        LessonPlanResumingScreen(lessonPlan: lessonPlan).map { screen in
+            { navigator.go(to: screen, on: currentScreen) }
+        }
+    }
+
+    private var showCancelLessonPlanFormAction: Action? {
+        LessonPlanCancellationScreen(lessonPlan: lessonPlan).map { screen in
+            { navigator.go(to: screen, on: currentScreen) }
+        }
+    }
+}
