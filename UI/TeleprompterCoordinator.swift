3078f696e6d10c22c239ef48386775be8c4d604f
davdroman <d@vidroman.dev>
2021-08-17 14:55:46 +0100

    Move `TeleprompterCoordinator` to its own file

---
+++ UI/TeleprompterCoordinator.swift
@@ -0,0 +1,141 @@
+import SwiftUISugar
+
+final class TeleprompterCoordinator {
+    enum Importance: Equatable {
+        case transient
+        case prominent
+    }
+
+    enum Transition: Equatable {
+        case moveWithOpacity
+        case opacity
+        static let `default` = moveWithOpacity
+
+        var anyTransition: AnyTransition {
+            switch self {
+            case .moveWithOpacity:
+                return .offset(y: 70) + .opacity
+            case .opacity:
+                return .opacity
+            }
+        }
+    }
+
+    // TODO: replace with AttributedString in iOS 15
+    struct TextElement: Equatable {
+        var style: Font.RythmicoTextStyle? = nil
+        let string: String
+    }
+
+    init(initialDelay: Double = 0) {
+        self.initialDelay = initialDelay
+        self._compoundedDelay = .init(currentValue: initialDelay)
+    }
+
+    private let initialDelay: Double
+    @RetainOldValue
+    private var compoundedDelay: Double
+
+    @ViewBuilder
+    func view<Content: View>(
+        transition: Transition = .default,
+        importance: Importance,
+        @ViewBuilder content: () -> Content
+    ) -> some View {
+        content().modifier(transitionModifier(transition, importance: importance))
+    }
+
+    @ViewBuilder
+    func text(
+        transition: Transition = .default,
+        separator: String = .empty,
+        style: Font.RythmicoTextStyle,
+        lineLimit: Int? = nil,
+        @ArrayBuilder<TextElement> _ elements: () -> [TextElement]
+    ) -> some View {
+        let elements = elements()
+        elements
+            .map { Text($0.string).rythmicoFontWeight($0.style ?? style) }
+            .joined(separator: Text(separator))
+            .rythmicoTextStyle(style)
+            .lineLimit(lineLimit)
+            .minimumScaleFactor(lineLimit == nil ? 1 : 0.5)
+            .modifier(transitionModifier(transition, for: elements))
+    }
+
+    private func transitionModifier(_ transition: Transition, importance: Importance) -> some ViewModifier {
+        switch importance {
+        case .transient:
+            return transitionModifier(transition, importance: importance, compoundingDelay: 0.5)
+        case .prominent:
+            return transitionModifier(transition, importance: importance, compoundingDelay: 1)
+        }
+    }
+
+    private func transitionModifier(_ transition: Transition, for elements: [TextElement]) -> some ViewModifier {
+        let wordCount = Double(elements.map(\.string).joined().words.count)
+        let avgReadingSpeed: Double = 200/60 // avg words per minute / 60 seconds = avg words per second
+        let delay = wordCount / avgReadingSpeed
+        return transitionModifier(transition, importance: nil, compoundingDelay: delay)
+    }
+
+    private func transitionModifier(_ transition: Transition, importance: Importance?, compoundingDelay delay: Double) -> some ViewModifier {
+        let deferred: Action
+        if importance == .transient {
+            let oldCompoundedDelay = compoundedDelay
+            let newCompoundedDelay = ($compoundedDelay ?? initialDelay) + delay
+            compoundedDelay = newCompoundedDelay
+            deferred = { self.compoundedDelay = oldCompoundedDelay + delay }
+        } else {
+            deferred = { self.compoundedDelay += delay }
+        }
+        defer { deferred() }
+        return OnAppearTransitionModifier(
+            transition: transition.anyTransition,
+            animation: .rythmicoSpring(duration: .durationMedium).delay(compoundedDelay)
+        )
+    }
+}
+
+extension TeleprompterCoordinator.TextElement: ExpressibleByStringLiteral {
+    init(stringLiteral value: String) {
+        self.init(style: .none, string: value)
+    }
+}
+
+func <- (lhs: String, rhs: Font.RythmicoTextStyle) -> TeleprompterCoordinator.TextElement {
+    .init(style: rhs, string: lhs)
+}
+
+@propertyWrapper
+private struct RetainOldValue<T> {
+    /// Old value
+    private(set) var projectedValue: T? = nil
+    /// Current value
+    var wrappedValue: T {
+        willSet {
+            projectedValue = wrappedValue
+        }
+    }
+
+    init(currentValue: T) {
+        self.wrappedValue = currentValue
+    }
+}
+
+private struct OnAppearTransitionModifier: ViewModifier {
+    let transition: AnyTransition
+    let animation: Animation?
+
+    @State private var appeared = false
+
+    func body(content: Content) -> some View {
+        ZStack {
+            if appeared {
+                content.transition(transition)
+            }
+        }
+        .animation(animation, value: appeared)
+        .onAppear { appeared = true }
+    }
+}
