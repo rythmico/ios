5d18767b64fe69e1f93866d4358a7810ca37d989
davdroman <d@vidroman.dev>
2021-08-18 16:12:17 +0100

    Introduce `URLOpener.openAppStorePageToWriteReview`

--- Services/URLOpener/URLOpener+Rythmico.swift
+++ Services/URLOpener/URLOpener+Rythmico.swift
@@ -4,4 +4,8 @@ extension URLOpener {
     func openTermsAndConditionsURL() {
         open("https://rythmico.com/terms-and-policies")
     }
+
+    func openAppStorePageToWriteReview() {
+        open("https://apps.apple.com/app/id1493135894?action=write-review")
+    }
 }
