776812b32d8107a0dedada491e5a18b536d18978
davdroman <d@vidroman.dev>
2021-08-19 16:20:37 +0100

    Introduce `ConditionalModifiers` SwiftUI package

---
+++ Packages/SwiftUISugar/Sources/SwiftUISugar/ConditionalModifiers/IfElse.swift
@@ -0,0 +1,14 @@
+extension View {
+    @ViewBuilder
+    public func `if`<TrueContent: View, FalseContent: View>(
+        _ condition: Bool,
+        if ifTransform: (Self) -> TrueContent,
+        else elseTransform: (Self) -> FalseContent
+    ) -> some View {
+        if condition {
+            ifTransform(self)
+        } else {
+            elseTransform(self)
+        }
+    }
+}
