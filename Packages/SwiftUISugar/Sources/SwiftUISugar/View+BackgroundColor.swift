92b08d48bbbfff9abf96c71c1083e9aa12b50589
davdroman <d@vidroman.dev>
2021-08-19 13:04:34 +0100

    Disable implicit animations for `backgroundColor` modifier

--- Packages/SwiftUISugar/Sources/SwiftUISugar/View+BackgroundColor.swift
+++ Packages/SwiftUISugar/Sources/SwiftUISugar/View+BackgroundColor.swift
@@ -1,5 +1,5 @@
 extension View {
     public func backgroundColor(_ color: Color) -> some View {
-        self.background(color.edgesIgnoringSafeArea(.all))
+        self.background(color.edgesIgnoringSafeArea(.all).animation(.none))
     }
 }
