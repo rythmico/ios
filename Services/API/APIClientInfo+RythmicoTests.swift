364891c1e709805d2fccccead007c9e24883bf4f
davdroman <d@vidroman.dev>
2021-08-24 00:20:58 +0100

    Improve `APIClientInfo` test coverage

--- Services/API/APIClientInfo+RythmicoTests.swift
+++ Services/API/APIClientInfo+RythmicoTests.swift
@@ -3,11 +3,13 @@ import FoundationSugar
 @testable import Rythmico
 
 final class APIClientInfoRythmicoTests: XCTestCase {
-    func test() {
+    func test() throws {
         let info = APIClientInfo.current
         XCTAssertEqual(info.keys.count, 3)
         XCTAssertEqual(info["Client-Id"], "com.rythmico.student")
-        XCTAssertNotNil(info["Client-Version"].flatMap(Version.init))
-        XCTAssertNotNil(info["Client-Build"].flatMap(Int.init))
+        let version = try XCTUnwrap(info["Client-Version"].flatMap(Version.init))
+        XCTAssert(version > .null)
+        let build = try XCTUnwrap(info["Client-Build"].flatMap(Int.init))
+        XCTAssert(build > 0)
     }
 }
