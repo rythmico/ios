91961b520123a6d097a298bd07ccde71798d32ee
davdroman <d@vidroman.dev>
2021-08-26 04:45:16 +0100

    Generalize `ListFormatter.string` function

--- Packages/FoundationEncore/Sources/FoundationEncore/ListFormatter+Sugar.swift
+++ Packages/FoundationEncore/Sources/FoundationEncore/ListFormatter+Sugar.swift
@@ -1,5 +1,5 @@
 extension ListFormatter {
-    public func string(from strings: [String]) -> String {
+    public func string<C>(from strings: C) -> String where C: RandomAccessCollection, C.Element: StringProtocol {
         guard let string = string(from: strings.map { $0 as Any }) else {
             preconditionFailure("ListFormatter failed to list strings: '\(strings)'")
         }
