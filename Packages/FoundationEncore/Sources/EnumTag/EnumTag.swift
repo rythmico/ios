f5c872877f4535c8e2954a1821461e75209d9b8d
davdroman <d@vidroman.dev>
2021-08-26 04:56:07 +0100

    Acknowledge `EnumTag`'s original source

--- Packages/FoundationEncore/Sources/EnumTag/EnumTag.swift
+++ Packages/FoundationEncore/Sources/EnumTag/EnumTag.swift
@@ -1,3 +1,27 @@
+// MIT License
+//
+// Copyright (c) 2020 Point-Free, Inc.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+// https://github.com/pointfreeco/swift-composable-architecture/blob/3c51885b983b623f0cc506660d79b02d45a8ffc1/Sources/ComposableArchitecture/SwiftUI/SwitchStore.swift#L1199-L1215
+
 public func enumTag<Case>(_ `case`: Case) -> UInt32? {
     let metadataPtr = unsafeBitCast(type(of: `case`), to: UnsafeRawPointer.self)
     let kind = metadataPtr.load(as: Int.self)
