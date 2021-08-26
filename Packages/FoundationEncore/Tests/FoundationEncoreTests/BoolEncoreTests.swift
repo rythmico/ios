1a82506c6a37246ed1c8acab78f496a16c3174c4
davdroman <d@vidroman.dev>
2021-08-26 19:35:43 +0100

    Expand `BoolEncore` tests

--- Packages/FoundationEncore/Tests/FoundationEncoreTests/BoolEncoreTests.swift
+++ Packages/FoundationEncore/Tests/FoundationEncoreTests/BoolEncoreTests.swift
@@ -6,4 +6,18 @@ final class BoolEncoreTests: XCTestCase {
         XCTAssertTrue(false.not)
         XCTAssertFalse(true.not)
     }
+
+    func testIsTrue() {
+        XCTAssertTrue(true.isTrue)
+        XCTAssertFalse(false.isTrue)
+    }
+
+    func testIsFalse() {
+        XCTAssertTrue(false.isFalse)
+        XCTAssertFalse(true.isFalse)
+    }
+
+    func testAllCases() {
+        XCTAssertEqual(Bool.allCases, [false, true])
+    }
 }
