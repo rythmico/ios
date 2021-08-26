7743200dc9d533964f07d4fd213532883d74545b
davdroman <d@vidroman.dev>
2021-08-26 04:47:04 +0100

    Add TODO

--- Packages/FoundationEncore/Sources/FoundationEncore/DispatchQueue+Sugar.swift
+++ Packages/FoundationEncore/Sources/FoundationEncore/DispatchQueue+Sugar.swift
@@ -1,3 +1,4 @@
+// TODO: remove when adopting TCA, use mock schedulers instead wherever needed.
 extension DispatchQueue {
     // Optimized for immediate main thread dispatching.
     public func nowOrAsync(execute work: @escaping @convention(block) () -> Void) {
