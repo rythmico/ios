ea0b6b90ec9e8336ebb9b0db7585454c13a5ac0f
davdroman <d@vidroman.dev>
2021-08-27 07:12:07 +0100

    Introduce `UUID.incrementing` factory function

--- Packages/FoundationEncore/Sources/FoundationEncore/UUIDEncore.swift
+++ Packages/FoundationEncore/Sources/FoundationEncore/UUIDEncore.swift
@@ -1,3 +1,19 @@
 extension UUID {
-    public static var zero: Self { .init(uuidString: "00000000-0000-0000-0000-000000000000")! }
+    /// 00000000-0000-0000-0000-000000000000
+    public static var zero: UUID { UUID(uuidInt: 0) }
+
+    /// A deterministic, auto-incrementing UUID factory function.
+    public static var incrementing: () -> UUID {
+        var uuidInt = 0
+        return {
+            defer { uuidInt += 1 }
+            return UUID(uuidInt: uuidInt)
+        }
+    }
+
+    private init(uuidInt int: Int) {
+        self = UUID(uuidString: "00000000-0000-0000-0000-\(String(format: "%012x", int))") !! preconditionFailure(
+            "Failed to create UUID with integer \(int)"
+        )
+    }
 }
