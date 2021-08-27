3e1b8a15ce649b8fd463cd46c6b7b1144e195f2e
davdroman <d@vidroman.dev>
2021-08-27 05:19:41 +0100

    Improve error descriptions for `URL.init(scheme:doubleSlash:host:path:queryItems:)`

--- Packages/FoundationEncore/Sources/FoundationEncore/URLEncore.swift
+++ Packages/FoundationEncore/Sources/FoundationEncore/URLEncore.swift
@@ -1,7 +1,16 @@
 extension URL {
-    public enum Error: Swift.Error {
-        case invalidURLComponents
-        case schemeDoubleSlashRemovalFailed
+    public enum Error: LocalizedError {
+        case invalidURLComponents(URLComponents)
+        case schemeDoubleSlashRemovalFailed(URL)
+
+        public var errorDescription: String? {
+            switch self {
+            case .invalidURLComponents(let urlComponents):
+                return "Failed to initialize URL with components: \(urlComponents)"
+            case .schemeDoubleSlashRemovalFailed(let url):
+                return "Failed to remove double slash from URL: \(url)"
+            }
+        }
     }
 
     public init(
@@ -11,18 +20,18 @@ extension URL {
         path: String = "",
         queryItems: [URLQueryItem]? = nil
     ) throws {
-        let url = try (URLComponents() => {
+        let urlComponents = URLComponents() => {
             $0.scheme = scheme
             $0.host = host
             $0.path = path
             $0.queryItems = queryItems
-        })
-        .url !! Error.invalidURLComponents
+        }
+        let url = try urlComponents.url !! Error.invalidURLComponents(urlComponents)
         self = doubleSlash ? url : try url.removingSchemeDoubleSlash()
     }
 
     private func removingSchemeDoubleSlash() throws -> URL {
-        try URL(string: absoluteString.replacingOccurrences(of: "://", with: ":")) !! Error.schemeDoubleSlashRemovalFailed
+        try URL(string: absoluteString.replacingOccurrences(of: "://", with: ":")) !! Error.schemeDoubleSlashRemovalFailed(self)
     }
 }
 
