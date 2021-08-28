03ab9ee8257004fbad670fa0cc2631d834cd99ba
davdroman <d@vidroman.dev>
2021-08-28 02:50:36 +0100

    Gardening

--- Packages/SwiftUIEncore/Sources/SwiftUIEncore/EdgeInsets+Sugar.swift
+++ Packages/SwiftUIEncore/Sources/SwiftUIEncore/EdgeInsets+Sugar.swift
@@ -88,3 +88,17 @@ extension EdgeInsets {
         }
     }
 }
+
+extension View {
+    public func padding(_ horizontal: HorizontalInsets, _ vertical: VerticalInsets) -> some View {
+        self.padding(EdgeInsets(horizontal: horizontal, vertical: vertical))
+    }
+
+    public func padding(_ horizontal: HorizontalInsets) -> some View {
+        self.padding(EdgeInsets(horizontal: horizontal))
+    }
+
+    public func padding(_ vertical: VerticalInsets) -> some View {
+        self.padding(EdgeInsets(vertical: vertical))
+    }
+}
