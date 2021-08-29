8c24e864bd4b6f7e6e8c7cb72003793550ac1f66
davdroman <d@vidroman.dev>
2021-08-29 16:13:31 +0100

    Improve `UUID.incrementing` test case

--- Packages/FoundationEncore/Tests/FoundationEncoreTests/UUIDEncoreTests.swift
+++ Packages/FoundationEncore/Tests/FoundationEncoreTests/UUIDEncoreTests.swift
@@ -8,20 +8,21 @@ final class UUIDEncoreTests: XCTestCase {
     }
 
     func testIncrementing() {
-        let uuid = UUID.incrementing
-        XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000000")
-        XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000001")
-        XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000002")
-        XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000003")
-        XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000004")
-        XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000005")
-        XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000006")
-        XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000007")
-        XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000008")
-        XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000009")
-        XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-00000000000A")
-        XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-00000000000B")
-        XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-00000000000C")
-        XCTAssertNotEqual(uuid(), uuid())
+        [UUID.incrementing, UUID.incrementing].forEach { uuid in
+            XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000000")
+            XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000001")
+            XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000002")
+            XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000003")
+            XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000004")
+            XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000005")
+            XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000006")
+            XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000007")
+            XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000008")
+            XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-000000000009")
+            XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-00000000000A")
+            XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-00000000000B")
+            XCTAssertEqual(uuid().uuidString, "00000000-0000-0000-0000-00000000000C")
+            XCTAssertNotEqual(uuid(), uuid())
+        }
     }
 }
