23ac5cb47e927141b445a2d76db4454b53ce377d
davdroman <d@vidroman.dev>
2021-10-15 12:39:21 +0100

    Simplify `ScrollView` proxy init implementation

--- Packages/SwiftUIEncore/Sources/SwiftUIEncore/ScrollViewEncore.swift
+++ Packages/SwiftUIEncore/Sources/SwiftUIEncore/ScrollViewEncore.swift
@@ -1,13 +1,11 @@
-extension ScrollView where Content == ScrollViewReader<AnyView> {
+extension ScrollView {
     public init<InnerContent: View>(
         _ axes: Axis.Set = .vertical,
         showsIndicators: Bool = true,
         @ViewBuilder content: @escaping (ScrollViewProxy) -> InnerContent
-    ) {
+    ) where Content == ScrollViewReader<InnerContent> {
         self.init(axes, showsIndicators: showsIndicators) {
-            ScrollViewReader { proxy in
-                content(proxy).eraseToAnyView()
-            }
+            ScrollViewReader(content: content)
         }
     }
 }
